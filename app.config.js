a7b9e96570bdd2979fabb29fb0691175337631c2
nmalzieu <noe.malzieu@gmail.com>
2022-12-16 12:56:45 +0100

    Add the xmtp preview url

---
+++ app.config.js
@@ -0,0 +1,47 @@
+export default {
+  name: process.env.PRODUCTION ? "Converse" : "Converse PREVIEW",
+  scheme: "converse",
+  slug: "converse",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+    url: "https://u.expo.dev/49a65fae-3895-4487-8e8a-5bd8bee3a401",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: process.env.PRODUCTION
+      ? "com.converse.native"
+      : "com.converse.preview",
+    buildNumber: "7",
+    config: {
+      usesNonExemptEncryption: false,
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: "49a65fae-3895-4487-8e8a-5bd8bee3a401",
+    },
+  },
+  runtimeVersion: {
+    policy: "sdkVersion",
+  },
+  owner: "noemalzieu",
+};
--- config.ts
+++ config.ts
@@ -7,6 +7,11 @@ const ENV = {
       "https://b752-2a01-cb04-85e-2800-997-a129-dcce-570.eu.ngrok.io",
     notificationsServerURI: "http://noe-mbp.local:9875",
   },
+  preview: {
+    xmtpEnv: "dev",
+    xmtpWebviewURI: "https://xmtp-native-webview.vercel.app",
+    notificationsServerURI: "https://xmtp-notifications-preview.fly.dev",
+  },
   prod: {
     xmtpEnv: "production",
     xmtpWebviewURI: "https://xmtp-native-webview.vercel.app",
@@ -17,8 +22,10 @@ const ENV = {
 const getConfig = (env = Constants.manifest?.releaseChannel) => {
   if (__DEV__) {
     return ENV.dev;
-  } else {
+  } else if (env === "production") {
     return ENV.prod;
+  } else {
+    return ENV.preview;
   }
 };
 
