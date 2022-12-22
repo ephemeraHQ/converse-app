26a146d0cf4986fbd60af92910d09530672e55e3
nmalzieu <noe.malzieu@gmail.com>
2022-12-22 13:18:44 +0100

    Add update mechanism to debug button

--- components/DebugButton.tsx
+++ components/DebugButton.tsx
@@ -1,12 +1,11 @@
 import { useActionSheet } from "@expo/react-native-action-sheet";
 import Constants from "expo-constants";
+import * as Updates from "expo-updates";
 import { useContext } from "react";
 import { Button, View } from "react-native";
 
 import config from "../config";
 import { clearDB } from "../data/db";
-import dataSource from "../data/db/datasource";
-import { Conversation } from "../data/db/entities/conversation";
 import { AppContext } from "../data/store/context";
 
 const logs: string[] = [];
@@ -24,17 +23,24 @@ export default function DebugButton() {
         onPress={() => {
           const methods: any = {
             "Clear DB": clearDB,
+            "Update app": async () => {
+              try {
+                const update = await Updates.fetchUpdateAsync();
+                if (update.isNew) {
+                  await Updates.reloadAsync();
+                } else {
+                  alert("No new update");
+                }
+              } catch (error) {
+                alert(error);
+                console.error(error);
+              }
+            },
             "Show logs": () => {
               alert(logs.join("\n"));
             },
             "Show state": () => {
               alert(JSON.stringify(state, null, 2));
-            },
-            "Show db": async () => {
-              const convosInDb = await dataSource
-                .getRepository(Conversation)
-                .count();
-              alert(JSON.stringify({ convosCount: convosInDb }, null, 2));
             },
             "Show config": () => {
               alert(
