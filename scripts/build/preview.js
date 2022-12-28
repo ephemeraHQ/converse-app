0a9da4cf07639653c6bd0c4268bbabffc3608228
nmalzieu <noe.malzieu@gmail.com>
2022-12-28 13:44:06 +0100

    Dev build

---
+++ scripts/build/preview.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const isClean = require("git-is-clean");
+const plist = require("plist");
+
+const go = async () => {
+  const clean = await isClean();
+  if (!clean) {
+    console.log("Git index is not clean");
+    process.exit(1);
+  }
+
+  const PROJ_PATH = "ios/Converse.xcodeproj/project.pbxproj";
+  const projContent = fs.readFileSync(PROJ_PATH, "utf-8");
+
+  const newProjContent = projContent
+    .replace(
+      /PRODUCT_BUNDLE_IDENTIFIER = com\.converse\.native/g,
+      "PRODUCT_BUNDLE_IDENTIFIER = com.converse.preview"
+    )
+    .replace(
+      /com\.converse\.native AppStore 2022-12-08T13:34:24\.744Z/g,
+      "com.converse.preview AdHoc 1671190919466"
+    )
+    .replace(
+      /ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon/g,
+      "ASSETCATALOG_COMPILER_APPICON_NAME = AppIconPreview"
+    );
+
+  fs.writeFileSync(PROJ_PATH, newProjContent);
+
+  const PLIST_PATH = "ios/Converse/Info.plist";
+
+  const info = plist.parse(fs.readFileSync(PLIST_PATH, "utf8"));
+  info.CFBundleDisplayName = "Converse PREVIEW";
+  info.CFBundleURLSchemes = ["converse-preview", "com.converse.preview"];
+  const newInfo = plist.build(info);
+  fs.writeFileSync(PLIST_PATH, newInfo, "utf-8");
+};
+
+go();
