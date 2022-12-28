1ab692df4c3677add153a563e0224d2201a275d2
nmalzieu <noe.malzieu@gmail.com>
2022-12-28 18:05:08 +0100

    Working Dev Extension

--- ios/ConverseNotificationExtension/NotificationService.swift
+++ ios/ConverseNotificationExtension/NotificationService.swift
@@ -8,28 +8,42 @@
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
-
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
+  
+  var contentHandler: ((UNNotificationContent) -> Void)?
+  var bestAttemptContent: UNMutableNotificationContent?
+  
+  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    self.contentHandler = contentHandler
+    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+    let sharedDefaults = UserDefaults(suiteName: "group.com.converse")
     
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
+    
+    if let bestAttemptContent = bestAttemptContent {
+      
+      if let body = bestAttemptContent.userInfo["body"] as? [String: Any], let contentTopic = body["contentTopic"] as? String {
+        let conversationDictString = sharedDefaults?.string(forKey: "conversation-\(contentTopic)")
+        if let data = conversationDictString?.data(using: .utf8) {
+          do {
+            if let conversationDict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+              let shortAddress = conversationDict["shortAddress"]
+              let lensHandle = conversationDict["lensHandle"]
+              bestAttemptContent.title = "\(lensHandle ?? (shortAddress ?? bestAttemptContent.title))"
+            }
+          } catch {
+          }
         }
+      }
+      
+      contentHandler(bestAttemptContent)
     }
-
+  }
+  
+  override func serviceExtensionTimeWillExpire() {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+      contentHandler(bestAttemptContent)
+    }
+  }
+  
 }
