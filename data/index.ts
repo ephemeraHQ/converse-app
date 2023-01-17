c4cc2d4ba0111775c579f8f201e758547d97c77a
nmalzieu <noe.malzieu@gmail.com>
2023-01-17 22:22:44 +1100

    Add logs on message error

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import SharedGroupPreferences from "react-native-shared-group-preferences";
 
-import { addLog } from "../components/DebugButton";
+// import { addLog } from "../components/DebugButton";
 import { resolveENSAddress } from "../utils/ens";
 import { getLensHandle } from "../utils/lens";
 import { shortAddress } from "../utils/str";
@@ -192,14 +192,14 @@ export const loadDataToContext = async (dispatch: DispatchType) => {
     order: { messages: { sent: "ASC" } },
   });
 
-  conversationsWithMessages.forEach((c) => {
-    SharedGroupPreferences.getItem(
-      `conversation-${c.topic}`,
-      "group.com.converse"
-    ).then((value) => {
-      addLog(JSON.stringify(value));
-    });
-  });
+  // conversationsWithMessages.forEach((c) => {
+  //   SharedGroupPreferences.getItem(
+  //     `conversation-${c.topic}`,
+  //     "group.com.converse"
+  //   ).then((value) => {
+  //     addLog(JSON.stringify(value));
+  //   });
+  // });
 
   dispatch({
     type: XmtpDispatchTypes.XmtpSetConversations,
