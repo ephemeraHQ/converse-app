3a292ddd7328482b89bcae0a16a12d4c7ea0345e
nmalzieu <noe.malzieu@gmail.com>
2023-01-23 10:29:07 +1100

    Decode notification from Swift

--- ios/ConverseNotificationExtension/NotificationService.swift
+++ ios/ConverseNotificationExtension/NotificationService.swift
@@ -7,6 +7,100 @@
 
 import UserNotifications
 import KeychainAccess
+import XMTP
+
+func getXmtpClientFromKeys() -> XMTP.Client? {
+  let keychain = Keychain(service: "converse.keychainService")
+  let xmtpKeys = keychain["XMTP_KEYS"]
+  if (xmtpKeys == nil) {
+    return nil;
+  }
+  let decoder = JSONDecoder()
+  let decoded = try! decoder.decode([UInt8].self, from: xmtpKeys!.data(using: .utf8)!)
+  let data = Data(decoded)
+  let privateKeyBundle = try! PrivateKeyBundle(serializedData: data)
+  let xmtpEnv = getXmtpEnv()
+  let client = try! Client.from(bundle: privateKeyBundle, options: .init(api: .init(env: xmtpEnv)))
+  return client
+}
+
+func getXmtpEnv() -> XMTP.XMTPEnvironment {
+  let sharedDefaults = UserDefaults(suiteName: "group.com.converse")
+  let xmtpEnvString = sharedDefaults?.string(forKey: "xmtp-env")
+  if (xmtpEnvString == "production") {
+    return .production;
+  } else {
+    return .dev;
+  }
+}
+
+func getSavedConversationTitle(contentTopic: String)-> String {
+  let sharedDefaults = UserDefaults(suiteName: "group.com.converse")
+  let conversationDictString = sharedDefaults?.string(forKey: "conversation-\(contentTopic)")
+  if let data = conversationDictString?.data(using: .utf8) {
+    if let conversationDict = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+      let shortAddress = conversationDict["shortAddress"]
+      let lensHandle = conversationDict["lensHandle"]
+      let ensName = conversationDict["ensName"]
+      return "\(lensHandle ?? (ensName ?? (shortAddress ?? "")))"
+    }
+  }
+  return "";
+}
+
+func decodeConversationMessage(xmtpClient: XMTP.Client, contentTopic: String, encodedMessage: String) async -> String? {
+  let persistence = Persistence()
+  var conversationContainer = try! persistence.load(conversationTopic: contentTopic)
+  if (conversationContainer == nil) {
+    let conversations = try! await xmtpClient.conversations.list()
+    for conversation in conversations {
+      do {
+        try Persistence().save(conversation: conversation)
+        conversationContainer = try! persistence.load(conversationTopic: contentTopic)
+      } catch {
+        print("Error saving \(conversation.topic): \(error)")
+      }
+    }
+  }
+  
+  if (conversationContainer != nil) {
+    let conversation = conversationContainer!.decode(with: xmtpClient)
+    let encryptedMessageData = Data(base64Encoded: Data(encodedMessage.utf8))!
+    let envelope = XMTP.Envelope.with { envelope in
+      envelope.message = encryptedMessageData
+      envelope.contentTopic = contentTopic
+    }
+    
+    let decodedMessage = try! conversation.decode(envelope)
+    return try! decodedMessage.content()
+  }
+  return nil;
+}
+
+
+func handleNotificationAsync(contentHandler: ((UNNotificationContent) -> Void), bestAttemptContent: UNMutableNotificationContent?) async {
+  print("Received a notification!")
+  
+  if let bestAttemptContent = bestAttemptContent {
+    
+    if let body = bestAttemptContent.userInfo["body"] as? [String: Any], let contentTopic = body["contentTopic"] as? String, let encodedMessage = body["message"] as? String {
+      
+      let conversationTitle = getSavedConversationTitle(contentTopic: contentTopic);
+      bestAttemptContent.title = conversationTitle;
+      
+      let xmtpClient = getXmtpClientFromKeys();
+      
+      if (xmtpClient != nil) {
+        let messageContent = try! await decodeConversationMessage(xmtpClient: xmtpClient!, contentTopic: contentTopic, encodedMessage: encodedMessage)
+        if (messageContent != nil) {
+          bestAttemptContent.body = messageContent!;
+        }
+      }
+    }
+    
+    contentHandler(bestAttemptContent)
+  }
+}
 
 class NotificationService: UNNotificationServiceExtension {
   
@@ -16,32 +110,8 @@ class NotificationService: UNNotificationServiceExtension {
   override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
     self.contentHandler = contentHandler
     bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-    let sharedDefaults = UserDefaults(suiteName: "group.com.converse")
-    print("Received a notification!")
-    let keychain = Keychain(service: "converse.keychainService")
-    let token = keychain["XMTP_KEYS"]
-    print("token", token ?? "")
-    
-    
-    
-    if let bestAttemptContent = bestAttemptContent {
-      
-      if let body = bestAttemptContent.userInfo["body"] as? [String: Any], let contentTopic = body["contentTopic"] as? String {
-        let conversationDictString = sharedDefaults?.string(forKey: "conversation-\(contentTopic)")
-        if let data = conversationDictString?.data(using: .utf8) {
-          do {
-            if let conversationDict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
-              let shortAddress = conversationDict["shortAddress"]
-              let lensHandle = conversationDict["lensHandle"]
-              let ensName = conversationDict["ensName"]
-              bestAttemptContent.title = "\(lensHandle ?? (ensName ?? (shortAddress ?? bestAttemptContent.title)))"
-            }
-          } catch {
-          }
-        }
-      }
-      
-      contentHandler(bestAttemptContent)
+    Task {
+      await handleNotificationAsync(contentHandler: contentHandler, bestAttemptContent: bestAttemptContent);
     }
   }
   
@@ -49,6 +119,11 @@ class NotificationService: UNNotificationServiceExtension {
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
     if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+      if let body = bestAttemptContent.userInfo["body"] as? [String: Any], let contentTopic = body["contentTopic"] as? String {
+        let conversationTitle = getSavedConversationTitle(contentTopic: contentTopic);
+        bestAttemptContent.title = conversationTitle;
+      }
+      
       contentHandler(bestAttemptContent)
     }
   }
---
+++ ios/ConverseNotificationExtension/Persistence.swift
@@ -0,0 +1,37 @@
+//
+//  Persistence.swift
+//
+//
+//  Created by Pat Nakajima on 1/20/23.
+//
+
+import Foundation
+import KeychainAccess
+import XMTP
+
+struct Persistence {
+  var keychain: Keychain
+
+  init() {
+    keychain = Keychain(service: "converse.keychainService")
+  }
+
+  func load(conversationTopic: String) throws -> ConversationContainer? {
+    guard let data = try keychain.getData(key(topic: conversationTopic)) else {
+      return nil
+    }
+
+    let decoder = JSONDecoder()
+    let decoded = try decoder.decode(ConversationContainer.self, from: data)
+
+    return decoded
+  }
+
+  func save(conversation: Conversation) throws {
+    keychain[data: key(topic: conversation.topic)] = try JSONEncoder().encode(conversation.encodedContainer)
+  }
+
+  func key(topic: String) -> String {
+    "conversation-\(topic)"
+  }
+}
