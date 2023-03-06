9fa404e41fa40835815ca473a3607ecec6bc20e8
nmalzieu <noe.malzieu@gmail.com>
2023-03-06 12:15:44 +0100

    Fix background color flash

--- App.tsx
+++ App.tsx
@@ -2,7 +2,12 @@ import "reflect-metadata";
 import "./polyfills";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, useColorScheme, View } from "react-native";
+import {
+  ColorSchemeName,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from "react-native";
 // eslint-disable-next-line import/order
 import * as SplashScreen from "expo-splash-screen";
 import * as Sentry from "sentry-expo";
@@ -11,6 +16,7 @@ import XmtpWebview from "./components/XmtpWebview";
 import config from "./config";
 import { AppProvider } from "./data/store/context";
 import Main from "./screens/Main";
+import { backgroundColor } from "./utils/colors";
 
 SplashScreen.preventAutoHideAsync().catch(() => {
   /* reloading the app might trigger some race conditions, ignore them */
@@ -25,6 +31,7 @@ Sentry.init({
 
 export default function App() {
   const colorScheme = useColorScheme();
+  const styles = getStyles(colorScheme);
   return (
     <AppProvider>
       <View style={styles.safe}>
@@ -39,12 +46,14 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  safe: {
-    flex: 1,
-  },
-  hiddenXmtpWebviewContainer: {
-    flex: 0,
-    height: 0,
-  },
-});
+const getStyles = (colorScheme: ColorSchemeName) =>
+  StyleSheet.create({
+    safe: {
+      flex: 1,
+      backgroundColor: backgroundColor(colorScheme),
+    },
+    hiddenXmtpWebviewContainer: {
+      flex: 0,
+      height: 0,
+    },
+  });
