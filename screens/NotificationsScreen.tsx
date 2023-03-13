479f2e167cc6aed5d40907cc98dafe358b3e9cea
nmalzieu <noe.malzieu@gmail.com>
2023-03-13 14:24:33 +0100

    Fix notifications screen on dark mode

--- screens/NotificationsScreen.tsx
+++ screens/NotificationsScreen.tsx
@@ -1,14 +1,23 @@
 import React, { useContext } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  ColorSchemeName,
+} from "react-native";
 
 import Button from "../components/Button";
 import NotificationsSVG from "../components/svgs/notifications";
 import { AppContext } from "../data/store/context";
 import { NotificationsDispatchTypes } from "../data/store/notificationsReducer";
+import { textPrimaryColor } from "../utils/colors";
 import { requestPushNotificationsPermissions } from "../utils/notifications";
 
 export default function NotificationsScreen() {
   const { dispatch } = useContext(AppContext);
+  const colorScheme = useColorScheme();
+  const styles = getStyles(colorScheme);
   return (
     <View style={styles.notifications}>
       <View style={styles.picto}>
@@ -47,29 +56,32 @@ export default function NotificationsScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  notifications: {
-    flex: 1,
-    alignItems: "center",
-  },
-  picto: {
-    marginTop: 124,
-    marginBottom: 50,
-  },
-  title: {
-    fontWeight: "700",
-    fontSize: 34,
-  },
-  p: {
-    fontSize: 17,
-    marginLeft: 32,
-    marginRight: 32,
-    textAlign: "center",
-    marginTop: 21,
-    marginBottom: "auto",
-  },
-  later: {
-    marginBottom: 54,
-    marginTop: 21,
-  },
-});
+const getStyles = (colorScheme: ColorSchemeName) =>
+  StyleSheet.create({
+    notifications: {
+      flex: 1,
+      alignItems: "center",
+    },
+    picto: {
+      marginTop: 124,
+      marginBottom: 50,
+    },
+    title: {
+      fontWeight: "700",
+      fontSize: 34,
+      color: textPrimaryColor(colorScheme),
+    },
+    p: {
+      fontSize: 17,
+      marginLeft: 32,
+      marginRight: 32,
+      textAlign: "center",
+      marginTop: 21,
+      marginBottom: "auto",
+      color: textPrimaryColor(colorScheme),
+    },
+    later: {
+      marginBottom: 54,
+      marginTop: 21,
+    },
+  });
