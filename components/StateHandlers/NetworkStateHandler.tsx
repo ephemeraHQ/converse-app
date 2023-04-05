97486fd291cad76d3b2b709d79f0cf289c4ea5cd
nmalzieu <noe.malzieu@gmail.com>
2023-04-05 19:13:00 +0200

    Cleaner network state handler

--- components/StateHandlers/NetworkStateHandler.tsx
+++ components/StateHandlers/NetworkStateHandler.tsx
@@ -1,15 +1,21 @@
 import NetInfo from "@react-native-community/netinfo";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 import { AppDispatchTypes } from "../../data/store/appReducer";
 import { AppContext } from "../../data/store/context";
 
 export default function NetworkStateHandler() {
   const { state, dispatch } = useContext(AppContext);
+
+  const reachableRef = useRef(state.app.isInternetReachable);
+  useEffect(() => {
+    reachableRef.current = state.app.isInternetReachable;
+  }, [state.app.isInternetReachable]);
+
   useEffect(() => {
     const unsubscribeNetworkInfo = NetInfo.addEventListener((netState) => {
       const reachable = !!netState.isInternetReachable;
-      if (reachable !== state.app.isInternetReachable) {
+      if (reachable !== reachableRef.current) {
         console.log({ reachable });
         dispatch({
           type: AppDispatchTypes.AppSetInternetReachable,
@@ -21,6 +27,6 @@ export default function NetworkStateHandler() {
     return () => {
       unsubscribeNetworkInfo();
     };
-  }, [dispatch, state.app.isInternetReachable]);
+  }, [dispatch]);
   return null;
 }
