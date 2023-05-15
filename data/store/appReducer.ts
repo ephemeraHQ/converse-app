ee1276669be05a738de54ddf3eacabd048cb7e81
nmalzieu <noe.malzieu@gmail.com>
2023-05-15 16:26:27 +0200

    Remove showing action sheet reducer

--- data/store/appReducer.ts
+++ data/store/appReducer.ts
@@ -7,7 +7,6 @@ export type AppType = {
   mainIdentity: string;
   isInternetReachable: boolean;
   hydrationDone: boolean;
-  showingActionSheet: boolean;
 };
 
 export const appInitialState: AppType = {
@@ -15,7 +14,6 @@ export const appInitialState: AppType = {
   mainIdentity: "",
   isInternetReachable: false,
   hydrationDone: false,
-  showingActionSheet: false,
 };
 
 export enum AppDispatchTypes {
@@ -23,7 +21,6 @@ export enum AppDispatchTypes {
   AppSetMainIdentity = "APP_SET_MAIN_IDENTITY",
   AppSetInternetReachable = "APP_SET_INTERNET_REACHABLE",
   AppSetHydrationDone = "APP_SET_HYDRATION_DONE",
-  AppShowingActionSheet = "APP_SHOWING_ACTION_SHEET",
 }
 
 type AppPayload = {
@@ -38,9 +35,6 @@ type AppPayload = {
   };
   [AppDispatchTypes.AppSetHydrationDone]: {
     done: boolean;
-  };
-  [AppDispatchTypes.AppShowingActionSheet]: {
-    showing: boolean;
   };
 };
 
@@ -72,13 +66,6 @@ export const appReducer = (state: AppType, action: AppActions): AppType => {
       return {
         ...state,
         hydrationDone: action.payload.done,
-      };
-    }
-
-    case AppDispatchTypes.AppShowingActionSheet: {
-      return {
-        ...state,
-        showingActionSheet: action.payload.showing,
       };
     }
 
