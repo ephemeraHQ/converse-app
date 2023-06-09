f3bdfcd0c7f8af866f5c02d6188c0ff65f2a023f
nmalzieu <noe.malzieu@gmail.com>
2023-06-09 10:35:58 +0200

    Add regex

---
+++ utils/uns.ts
@@ -0,0 +1,26 @@
+export const ALLOWED_UNS_SUFFIXES = [
+  ".crypto",
+  ".bitcoin",
+  ".blockchain",
+  ".dao",
+  ".nft",
+  ".888",
+  ".wallet",
+  ".x",
+  ".klever",
+  ".zil",
+  ".hi",
+  ".kresus",
+  ".polygon",
+  ".anime",
+  ".manga",
+  ".binanceus",
+];
+
+export const isUNSAddress = (address: string): boolean => {
+  // Bail out early if empty string or a string without any dots
+  if (!address || !address.includes(".")) {
+    return false;
+  }
+  return ALLOWED_UNS_SUFFIXES.some((suffix) => address.endsWith(suffix));
+};
