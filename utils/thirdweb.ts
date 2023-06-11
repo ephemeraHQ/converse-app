fc311284021352325f36d2c25f02d3d789d1c8ce
nmalzieu <noe.malzieu@gmail.com>
2023-06-11 22:37:31 +0200

    Cleaner getIPFSAssetURI

--- utils/thirdweb.ts
+++ utils/thirdweb.ts
@@ -1,7 +1,9 @@
 export const getIPFSAssetURI = (ipfsURI?: string) => {
-  if (ipfsURI && ipfsURI.startsWith("ipfs://")) {
+  if (!ipfsURI) return ipfsURI;
+  if (ipfsURI.startsWith("ipfs://")) {
     return `https://ipfs.thirdwebcdn.com/ipfs/${ipfsURI.slice(7)}`;
-  } else if (ipfsURI && ipfsURI.startsWith("https://ipfs.infura.io/")) {
+  } else if (ipfsURI.startsWith("https://ipfs.infura.io/")) {
+    // The public infura gateway is now deprecated, using thirdweb's instead
     return ipfsURI.replace(
       "https://ipfs.infura.io/",
       "https://ipfs.thirdwebcdn.com/"
