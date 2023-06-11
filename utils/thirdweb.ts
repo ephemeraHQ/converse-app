a2ed703be903575ad11c3caeab920ac6538aca79
nmalzieu <noe.malzieu@gmail.com>
2023-06-11 22:27:10 +0200

    Replace infura gateway by thirdweb gateway

--- utils/thirdweb.ts
+++ utils/thirdweb.ts
@@ -1,6 +1,11 @@
 export const getIPFSAssetURI = (ipfsURI?: string) => {
   if (ipfsURI && ipfsURI.startsWith("ipfs://")) {
     return `https://ipfs.thirdwebcdn.com/ipfs/${ipfsURI.slice(7)}`;
+  } else if (ipfsURI && ipfsURI.startsWith("https://ipfs.infura.io/")) {
+    return ipfsURI.replace(
+      "https://ipfs.infura.io/",
+      "https://ipfs.thirdwebcdn.com/"
+    );
   }
   return ipfsURI;
 };
