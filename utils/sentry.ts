3e19ae8be64696a7ee44319b52a633ae92051b79
nmalzieu <noe.malzieu@gmail.com>
2023-07-17 12:19:13 +0200

    Handle errors with attachments

--- utils/sentry.ts
+++ utils/sentry.ts
@@ -1,6 +1,6 @@
 import * as Sentry from "sentry-expo";
 
-export const sentryTrackMessage = (message: string, extras: any) => {
+export const sentryTrackMessage = (message: string, extras = {} as any) => {
   Sentry.React.withScope((scope) => {
     scope.setExtras(extras);
     Sentry.React.captureMessage(message);
