cf2d71dea72aa57429c62e9061e36debb2fe2b53
nmalzieu <noe.malzieu@gmail.com>
2023-07-28 11:37:51 +0200

    Improved regex

--- utils/regex.ts
+++ utils/regex.ts
@@ -1,10 +1,22 @@
+const BOUNDARY_START = /(\s|\(|^)/.source;
+const BOUNDARY_END = /(\s|\)|$)/.source;
+const WORD_CONTENT = /[^()/\s]/.source;
+
+const getDomainExpression = (suffix: RegExp) =>
+  new RegExp(
+    `(?<=${BOUNDARY_START})${WORD_CONTENT}*${suffix.source}(?=${BOUNDARY_END})`,
+    "gi"
+  );
+
 export const ADDRESS_REGEX = /\b0x[a-fA-F0-9]{40}\b/gi;
-export const LENS_REGEX = /[^()\s]*\.lens\b/gi;
-export const ETH_REGEX = /[^()\s]*\.eth\b/gi;
-export const FARCASTER_REGEX = /[^()\s]*\.fc\b/gi;
-export const CB_ID_REGEX = /[^()\s]*\.cb\.id\b/gi;
-export const UNS_REGEX =
-  /[^()\s]*\.(crypto|bitcoin|blockchain|dao|nft|888|wallet|x|klever|zil|hi|kresus|polygon|anime|manga|binanceus)\b/gi;
+
+export const LENS_REGEX = getDomainExpression(/\.lens/);
+export const ETH_REGEX = getDomainExpression(/\.eth/);
+export const FARCASTER_REGEX = getDomainExpression(/\.fc/);
+export const CB_ID_REGEX = getDomainExpression(/\.cb\.id/);
+export const UNS_REGEX = getDomainExpression(
+  /\.(crypto|bitcoin|blockchain|dao|nft|888|wallet|x|klever|zil|hi|kresus|polygon|anime|manga|binanceus)/
+);
 
 export const EMAIL_REGEX =
   /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)*|\[((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|IPv6:((((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){6}|::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){5}|[0-9A-Fa-f]{0,4}::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){4}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):)?(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){3}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,2}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){2}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,3}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,4}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,5}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,6}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)|(?!IPv6:)[0-9A-Za-z-]*[0-9A-Za-z]:[!-Z^-~]+)])/gi;
