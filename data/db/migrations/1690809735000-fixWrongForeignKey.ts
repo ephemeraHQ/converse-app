bf178bb363330ac5469fa290448335d990e78b1a
nmalzieu <noe.malzieu@gmail.com>
2023-07-31 15:25:14 +0200

    Add a script to fix db

---
+++ data/db/migrations/1690809735000-fixWrongForeignKey.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class fixWrongForeignKey1690809735000 implements MigrationInterface {
+  name = "fixWrongForeignKey1690809735000";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "temporary_message" ("id" text PRIMARY KEY NOT NULL, "senderAddress" text NOT NULL, "sent" integer NOT NULL, "content" text NOT NULL, "conversationId" text NOT NULL, "status" text NOT NULL DEFAULT ('sent'), "sentViaConverse" boolean NOT NULL DEFAULT (0), "contentType" text NOT NULL DEFAULT ('xmtp.org/text:1.0'), "reactions" text NOT NULL DEFAULT ('{}'), "contentFallback" text, CONSTRAINT "FK_7cf4a4df1f2627f72bf6231635f" FOREIGN KEY ("conversationId") REFERENCES "conversation" ("topic") ON DELETE NO ACTION ON UPDATE NO ACTION);`
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_message"("id", "senderAddress", "sent", "content", "conversationId", "status", "sentViaConverse", "contentType", "reactions", "contentFallback") SELECT "id", "senderAddress", "sent", "content", "conversationId", "status", "sentViaConverse", "contentType", "reactions", "contentFallback" FROM "message";`
+    );
+    await queryRunner.query(`DROP TABLE "message";`);
+    await queryRunner.query(
+      `ALTER TABLE "temporary_message" RENAME TO "message";`
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_14fde3d0ed2191ef415d5f759d" ON "message" ("status")`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
