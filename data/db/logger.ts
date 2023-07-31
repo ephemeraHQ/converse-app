e697d5758497337388bc89848cf41c6c53ebb0ae
nmalzieu <noe.malzieu@gmail.com>
2023-07-31 23:03:34 +0200

    typeorm logs

---
+++ data/db/logger.ts
@@ -0,0 +1,44 @@
+import { Logger, QueryRunner } from "typeorm/browser";
+
+import { sentryTrackMessage } from "../../utils/sentry";
+
+export class TypeORMLogger implements Logger {
+  logQueryError(
+    error: string | Error,
+    query: string,
+    parameters?: any[] | undefined,
+    queryRunner?: QueryRunner | undefined
+  ) {
+    console.log("TYPEORM_QUERY_ERROR", { error, query, parameters });
+    sentryTrackMessage("TYPEORM_QUERY_ERROR", { error, query, parameters });
+  }
+
+  logQuerySlow(
+    time: number,
+    query: string,
+    parameters?: any[] | undefined,
+    queryRunner?: QueryRunner | undefined
+  ) {
+    // console.log(query);
+  }
+  logMigration(message: string, queryRunner?: QueryRunner | undefined) {
+    // console.log(message);
+  }
+  logQuery(
+    query: string,
+    parameters?: any[] | undefined,
+    queryRunner?: QueryRunner | undefined
+  ) {
+    // console.log(query);
+  }
+  log(
+    level: "log" | "info" | "warn",
+    message: any,
+    queryRunner?: QueryRunner | undefined
+  ) {
+    // console.log(message);
+  }
+  logSchemaBuild(message: string, queryRunner?: QueryRunner | undefined) {
+    // console.log(message);
+  }
+}
