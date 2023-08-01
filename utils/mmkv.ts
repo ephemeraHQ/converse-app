ba0bb4a84265bd518757b334c9cfa3198e22b6cf
nmalzieu <noe.malzieu@gmail.com>
2023-08-01 19:45:35 +0200

    Add a zustand mmkv storage

--- utils/mmkv.ts
+++ utils/mmkv.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MMKV } from "react-native-mmkv";
+import { StateStorage } from "zustand/middleware";
 
 const storage = new MMKV();
 
@@ -12,3 +13,16 @@ export const saveLastXMTPSyncedAt = (timestamp: number) => {
 
 export const getLastXMTPSyncedAt = () =>
   storage.getNumber("lastXMTPSyncedAt") || 0;
+
+export const zustandMMKVStorage: StateStorage = {
+  setItem: (name, value) => {
+    return storage.set(name, value);
+  },
+  getItem: (name) => {
+    const value = storage.getString(name);
+    return value ?? null;
+  },
+  removeItem: (name) => {
+    return storage.delete(name);
+  },
+};
