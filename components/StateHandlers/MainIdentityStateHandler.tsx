be10dd1ad4e2125b28a5bca721fcb4a5c1672361
nmalzieu <noe.malzieu@gmail.com>
2023-08-02 15:46:22 +0200

    Use main identity from local profile

--- components/StateHandlers/MainIdentityStateHandler.tsx
+++ components/StateHandlers/MainIdentityStateHandler.tsx
@@ -1,8 +1,8 @@
 import { useContext, useEffect } from "react";
 
-import { AppDispatchTypes } from "../../data/deprecatedStore/appReducer";
+import { refreshProfileForAddress } from "../../data";
 import { AppContext } from "../../data/deprecatedStore/context";
-import { getProfileForAddress, saveUser } from "../../utils/api";
+import { saveUser } from "../../utils/api";
 
 export default function MainIdentityStateHandler() {
   const { state, dispatch } = useContext(AppContext);
@@ -10,33 +10,7 @@ export default function MainIdentityStateHandler() {
   useEffect(() => {
     if (state.xmtp.address) {
       saveUser(state.xmtp.address);
-      getProfileForAddress(state.xmtp.address)
-        .then((result) => {
-          const primaryEns = result.ensNames?.find((e) => e.isPrimary)?.name;
-          if (primaryEns) {
-            dispatch({
-              type: AppDispatchTypes.AppSetMainIdentity,
-              payload: { identity: primaryEns },
-            });
-          } else {
-            dispatch({
-              type: AppDispatchTypes.AppSetMainIdentity,
-              payload: { identity: "" },
-            });
-          }
-        })
-        .catch((e) => {
-          console.log(e);
-          dispatch({
-            type: AppDispatchTypes.AppSetMainIdentity,
-            payload: { identity: "" },
-          });
-        });
-    } else {
-      dispatch({
-        type: AppDispatchTypes.AppSetMainIdentity,
-        payload: { identity: "" },
-      });
+      refreshProfileForAddress(state.xmtp.address);
     }
   }, [state.xmtp.address, dispatch]);
 
