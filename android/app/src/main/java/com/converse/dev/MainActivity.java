349840a90dd4f51ae3b746f1e6995b06146b1e18
nmalzieu <noe.malzieu@gmail.com>
2023-08-07 17:55:56 +0200

    Manual RN upgrade

--- android/app/src/main/java/com/converse/dev/MainActivity.java
+++ android/app/src/main/java/com/converse/dev/MainActivity.java
@@ -34,14 +34,11 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
+    return new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        ));
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 
   /**
