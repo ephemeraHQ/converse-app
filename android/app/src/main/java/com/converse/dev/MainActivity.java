945d193ed18651a792ff085ee4ff765a087db942
nmalzieu <noe.malzieu@gmail.com>
2023-08-08 10:07:47 +0200

    Fix expo dev client on android

--- android/app/src/main/java/com/converse/dev/MainActivity.java
+++ android/app/src/main/java/com/converse/dev/MainActivity.java
@@ -34,11 +34,11 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+        DefaultNewArchitectureEntryPoint.getFabricEnabled()));
   }
 
   /**
