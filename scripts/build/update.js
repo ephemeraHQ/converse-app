3568d926127191da62d2cb564db208dff112bebe
nmalzieu <noe.malzieu@gmail.com>
2023-08-14 08:58:53 +0200

    Update script

--- scripts/build/update.js
+++ scripts/build/update.js
@@ -2,24 +2,29 @@ const { spawn, execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const plist = require("plist");
-const prompt = require("prompt");
+const prompts = require("prompts");
 
 const IOS_UPDATE_ID_REGEX = /iOS update ID\s*(.*)/;
 const ANDROID_UPDATE_ID_REGEX = /Android update ID\s*(.*)/;
 
-const rootPath = path.join(__dirname, "..");
-const bundlesPath = path.join(__dirname, "../dist/bundles");
-const infoPlistPath = path.join(__dirname, "../ios/Converse/Info.plist");
-const buildGradlePath = path.join(__dirname, "../android/app/build.gradle");
+const rootPath = path.join(__dirname, "../..");
+const bundlesPath = path.join(__dirname, "../../dist/bundles");
+const infoPlistPath = path.join(__dirname, "../../ios/Converse/Info.plist");
+const buildGradlePath = path.join(__dirname, "../../android/app/build.gradle");
 
 const GRADLE_VERSION_NAME_REGEX = /versionName "(.*?)"/;
 const GRADLE_VERSION_CODE_REGEX = /versionCode ([0-9]*)/;
 
-const uploadUpdatesToExpo = () =>
+const uploadUpdatesToExpo = (env) =>
   new Promise((resolve) => {
     console.log("Uploading updates to Expo...");
-    const eas = spawn("eas", ["update", "--branch", "preview"], {
-      env: { EXPO_ENV: "preview", PATH: process.env.PATH },
+    const updateEnv = { PATH: process.env.PATH };
+    if (env === "preview") {
+      updateEnv.EXPO_ENV = "preview";
+    }
+
+    const eas = spawn("eas", ["update", "--branch", env], {
+      env: updateEnv,
       cwd: rootPath,
       stdio: ["inherit", "pipe", "pipe"],
     });
@@ -91,11 +96,14 @@ const uploadBundlesToSentry = ({
   iosMap,
   androidUpdateID,
   androidMap,
+  env,
 }) => {
+  const iosBundleId =
+    env === "preview" ? "com.converse.preview" : "com.converse.native";
   const iOSPlistData = plist.parse(fs.readFileSync(infoPlistPath, "utf8"));
   const androidBuildGradle = fs.readFileSync(buildGradlePath, "utf8");
   const sentryIOSUploadCommand = `SENTRY_ORG=converse-app SENTRY_PROJECT=converse-react-native SENTRY_AUTH_TOKEN=eac2788f667c4e35b610e664417b0acd1d2c43fd8ab2458986cfe85a96cf940a node_modules/@sentry/cli/bin/sentry-cli releases \
-  files com.converse.preview@${iOSPlistData.CFBundleShortVersionString}+${iOSPlistData.CFBundleVersion} \
+  files ${iosBundleId}@${iOSPlistData.CFBundleShortVersionString}+${iOSPlistData.CFBundleVersion} \
   upload-sourcemaps \
   --dist ${iosUpdateID} \
   --rewrite \
@@ -115,8 +123,10 @@ const uploadBundlesToSentry = ({
   const androidVersionCodeMatch = androidBuildGradle.match(
     GRADLE_VERSION_CODE_REGEX
   );
+  const androidBundleId =
+    env === "preview" ? "com.converse.preview" : "com.converse.prod";
   const sentryAndroidUploadCommand = `SENTRY_ORG=converse-app SENTRY_PROJECT=converse-react-native SENTRY_AUTH_TOKEN=eac2788f667c4e35b610e664417b0acd1d2c43fd8ab2458986cfe85a96cf940a node_modules/@sentry/cli/bin/sentry-cli releases \
-  files com.converse.preview@${androidVersionMatch[1].trim()}+${androidVersionCodeMatch[1].trim()} \
+  files ${androidBundleId}@${androidVersionMatch[1].trim()}+${androidVersionCodeMatch[1].trim()} \
   upload-sourcemaps \
   --dist ${androidUpdateID} \
   --rewrite \
@@ -130,10 +140,26 @@ const uploadBundlesToSentry = ({
   }
 };
 
-const go = async () => {
-  const { iosUpdateID, androidUpdateID } = await uploadUpdatesToExpo();
+const update = async () => {
+  const questions = [
+    {
+      type: "select",
+      name: "env",
+      message: "Pick an environment",
+      choices: [{ value: "preview" }, { value: "production" }],
+    },
+  ];
+  const { env } = await prompts(questions);
+  if (!env) process.exit(1);
+  const { iosUpdateID, androidUpdateID } = await uploadUpdatesToExpo(env);
   const { iosMap, androidMap } = copyBundlesAndGetMaps();
-  uploadBundlesToSentry({ iosUpdateID, iosMap, androidMap, androidUpdateID });
+  uploadBundlesToSentry({
+    iosUpdateID,
+    iosMap,
+    androidMap,
+    androidUpdateID,
+    env,
+  });
 };
 
-go();
+update();
