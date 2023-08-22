83737ce7d5f3fd9dbf18ca62ffb64a938120b47d
nmalzieu <noe.malzieu@gmail.com>
2023-08-22 16:21:07 +0200

    Fix not initiating local xmtp client during hydration

--- components/StateHandlers/HydrationStateHandler.tsx
+++ components/StateHandlers/HydrationStateHandler.tsx
@@ -4,13 +4,15 @@ import { loadDataToContext } from "../../data";
 import { initDb } from "../../data/db";
 import { cleanupPendingConversations } from "../../data/helpers/conversations/pendingConversations";
 import { refreshProfileForAddress } from "../../data/helpers/profiles/profilesUpdate";
-import { getAccounts, useUserStore } from "../../data/store/accountsStore";
+import {
+  getAccounts,
+  getUserStore,
+  useUserStore,
+} from "../../data/store/accountsStore";
 import { useAppStore } from "../../data/store/appStore";
 import { loadSavedNotificationMessagesToContext } from "../../utils/notifications";
 import { pick } from "../../utils/objects";
 import { getInstalledWallets } from "../Onboarding/supportedWallets";
-import { getLocalXmtpClient } from "../XmtpState";
-import { prepareForRefacto } from "./prepareForRefacto";
 
 export default function HydrationStateHandler() {
   const setUserAddress = useUserStore((s) => s.setUserAddress);
@@ -30,17 +32,10 @@ export default function HydrationStateHandler() {
       await loadSavedNotificationMessagesToContext();
       await Promise.all(accounts.map((a) => loadDataToContext(a)));
 
-      let address: string | null = null;
-
-      const xmtpClient = await getLocalXmtpClient();
-      if (xmtpClient) {
-        address = xmtpClient.address;
-        setUserAddress(xmtpClient.address);
-      }
-
-      if (address) {
-        accounts.map((a) => refreshProfileForAddress(a, address as string));
-      }
+      accounts.map((address) => {
+        getUserStore(address).getState().setUserAddress(address);
+        refreshProfileForAddress(address, address);
+      });
 
       setHydrationDone(true);
     };
