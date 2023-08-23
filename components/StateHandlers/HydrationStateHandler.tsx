b950dc0df17e06ab1546e0397f590abd21e9d131
nmalzieu <noe.malzieu@gmail.com>
2023-08-23 12:01:00 +0200

    removed unused methoid

--- components/StateHandlers/HydrationStateHandler.tsx
+++ components/StateHandlers/HydrationStateHandler.tsx
@@ -4,18 +4,13 @@ import { loadDataToContext } from "../../data";
 import { initDb } from "../../data/db";
 import { cleanupPendingConversations } from "../../data/helpers/conversations/pendingConversations";
 import { refreshProfileForAddress } from "../../data/helpers/profiles/profilesUpdate";
-import {
-  getAccounts,
-  getUserStore,
-  useUserStore,
-} from "../../data/store/accountsStore";
+import { getAccounts, getUserStore } from "../../data/store/accountsStore";
 import { useAppStore } from "../../data/store/appStore";
 import { loadSavedNotificationMessagesToContext } from "../../utils/notifications";
 import { pick } from "../../utils/objects";
 import { getInstalledWallets } from "../Onboarding/supportedWallets";
 
 export default function HydrationStateHandler() {
-  const setUserAddress = useUserStore((s) => s.setUserAddress);
   const { setHydrationDone } = useAppStore((s) =>
     pick(s, ["setHydrationDone"])
   );
