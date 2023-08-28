f8f5988e54aea6d1e47cee472a4e17c4a689cebf
nmalzieu <noe.malzieu@gmail.com>
2023-08-28 13:21:31 +0200

    Mmkv android extension

---
+++ android/app/src/main/java/com/converse/dev/Mmkv.kt
@@ -0,0 +1,15 @@
+package com.converse.dev
+import android.content.Context
+import android.util.Log
+import com.tencent.mmkv.MMKV;
+
+private var mmkvInstance:MMKV? = null;
+
+fun getMmkv(appContext: Context): MMKV? {
+    if (mmkvInstance != null) {
+        return mmkvInstance;
+    }
+    MMKV.initialize(appContext)
+    mmkvInstance = MMKV.defaultMMKV()
+    return mmkvInstance;
+}
