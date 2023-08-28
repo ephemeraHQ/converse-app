b83f4c0bbdc34308169269610acba3b82c89c76c
nmalzieu <noe.malzieu@gmail.com>
2023-08-28 10:59:19 +0200

    Add sqlite support to notification extension ios

--- ios/ConverseNotificationExtension/NotificationService.swift
+++ ios/ConverseNotificationExtension/NotificationService.swift
@@ -31,7 +31,6 @@ func handleNotificationAsync(contentHandler: ((UNNotificationContent) -> Void), 
   if let bestAttemptContent = bestAttemptContent {
     
     print("[NotificationExtension] Received a notification")
-    
     if var body = bestAttemptContent.userInfo["body"] as? [String: Any], let contentTopic = body["contentTopic"] as? String, let encodedMessage = body["message"] as? String {
       
       let xmtpClient = await getXmtpClientFromKeys();
--- ios/ConverseNotificationExtension/SQLite.swift
+++ ios/ConverseNotificationExtension/SQLite.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SQLite
+extension String: Error {}
+
+private var db: Connection? = nil;
+
+func getDb(account: String) throws -> Connection {
+  if let database = db {
+    return database
+  }
+  do {
+    let myGroupID: NSString = "group.com.converse.dev" // TODO => handle preview & prod
+    let groupDir = (FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: myGroupID as String)?.path)!
+    let dbPath = (groupDir as NSString).appendingPathComponent("converse-\(account).sqlite")
+    db = try Connection(dbPath)
+    if let database = db {
+      return database
+    }
+    throw "Could not connect to db"
+  } catch {
+    print (error)
+    throw "Could not connect to db"
+  }
+}
+
+func getConversations(account: String) throws {
+  let conversations = Table("conversation")
+  let topic = Expression<String>("topic")
+  let db = try getDb(account: account)
+  for conversation in try db.prepare(conversations) {
+      print("topic: \(conversation[topic])")
+  }
+}
