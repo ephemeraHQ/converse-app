9a08c6596adf7343787309a748b5ceef6f8bf344
nmalzieu <noe.malzieu@gmail.com>
2023-09-06 22:56:24 +0200

    Try to fix status bar color after bootsplash

--- utils/splash/splash.android.tsx
+++ utils/splash/splash.android.tsx
@@ -1,3 +1,18 @@
+import { Appearance } from "react-native";
 import RNBootSplash from "react-native-bootsplash";
 
-export const hideSplashScreen = () => RNBootSplash.hide();
+import { setAndroidColors } from "../colors";
+
+export const hideSplashScreen = async () => {
+  await RNBootSplash.hide();
+  // Fixing the status bar after hiding Splash Screen
+  // TODO => try to upgrade RNBootSplash and play
+  // with the XML values
+  setAndroidColors(Appearance.getColorScheme());
+  setTimeout(() => {
+    setAndroidColors(Appearance.getColorScheme());
+  }, 10);
+  setTimeout(() => {
+    setAndroidColors(Appearance.getColorScheme());
+  }, 100);
+};
