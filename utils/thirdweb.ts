a89655ab5fd88babf1fb7ce42ee1d8560978abd7
nmalzieu <noe.malzieu@gmail.com>
2023-09-06 18:07:31 +0200

    Use thirdweb gateway using ipfs

--- utils/thirdweb.ts
+++ utils/thirdweb.ts
@@ -1,13 +1,14 @@
+import config from "../config";
+
+const thirdwebGateway = `https://${config.thirdwebClientId}.ipfscdn.io`;
+
 export const getIPFSAssetURI = (ipfsURI?: string) => {
   if (!ipfsURI) return ipfsURI;
   if (ipfsURI.startsWith("ipfs://")) {
-    return `https://ipfs.thirdwebcdn.com/ipfs/${ipfsURI.slice(7)}`;
+    return `${thirdwebGateway}/ipfs/${ipfsURI.slice(7)}`;
   } else if (ipfsURI.startsWith("https://ipfs.infura.io/")) {
     // The public infura gateway is now deprecated, using thirdweb's instead
-    return ipfsURI.replace(
-      "https://ipfs.infura.io/",
-      "https://ipfs.thirdwebcdn.com/"
-    );
+    return ipfsURI.replace("https://ipfs.infura.io/", `${thirdwebGateway}/`);
   }
   return ipfsURI;
 };
