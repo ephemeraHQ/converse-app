e46b5b3728c7cdc7c6f5dfd14da8e88446cf736e
nmalzieu <noe.malzieu@gmail.com>
2023-09-13 11:24:28 +0200

    Fix the upsert method to save without returning

--- data/db/upsert.ts
+++ data/db/upsert.ts
@@ -28,14 +28,50 @@ export const checkUpsertSupport = async (dataSource: DataSource) => {
 export const upsertRepository = async <T extends ObjectLiteral>(
   repository: Repository<T>,
   entityOrEntities: QueryDeepPartialEntity<T> | QueryDeepPartialEntity<T>[],
-  conflictPathsOrOptions: string[]
+  conflictPathsOrOptions: string[],
+  returning = true
 ): Promise<any> => {
   if (supportsUpsert) {
-    const upsertResult = await repository.upsert(
-      entityOrEntities,
+    // This is basically the "upsert" method from typeorm but with the ability to not
+    // return the data from the query which will help in perf because SQLite does not have
+    // a "returning" parameter i.e. the ORM does queries right after to get back the data
+    const metadata = repository.metadata;
+    const conflictColumns = metadata.mapPropertyPathsToColumns(
       conflictPathsOrOptions
     );
-    return upsertResult;
+    let entities: QueryDeepPartialEntity<T>[];
+
+    if (!Array.isArray(entityOrEntities)) {
+      entities = [entityOrEntities];
+    } else {
+      entities = entityOrEntities;
+    }
+
+    const overwriteColumns = metadata.columns.filter(
+      (col) =>
+        !conflictColumns.includes(col) &&
+        entities.some(
+          (entity) => typeof col.getEntityValue(entity) !== "undefined"
+        )
+    );
+
+    const queryBuilder = repository.createQueryBuilder();
+    // This is the customized part compared to default typeorm feature
+    if (!returning) {
+      queryBuilder.expressionMap.updateEntity = false;
+    }
+
+    const result = await queryBuilder
+      .insert()
+      .values(entities)
+      .orUpdate(
+        [...conflictColumns, ...overwriteColumns].map(
+          (col) => col.databaseName
+        ),
+        conflictColumns.map((col) => col.databaseName)
+      )
+      .execute();
+    return result;
   } else {
     const entities = Array.isArray(entityOrEntities)
       ? entityOrEntities
