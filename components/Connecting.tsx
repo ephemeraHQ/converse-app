6a34b5ec7a4672ee055fe2dec93f37260526ce50
Louis <dev@louis.io>
2023-09-18 11:43:16 +0200

    Fix on Android: search placeholder connecting/syncing statuses, placeholder update on search while connecting/syncing

--- components/Connecting.tsx
+++ components/Connecting.tsx
@@ -6,7 +6,7 @@ import { textPrimaryColor } from "../utils/colors";
 import { pick } from "../utils/objects";
 import ActivityIndicator from "./ActivityIndicator/ActivityIndicator";
 
-const useShouldShowConnecting = () => {
+export const useShouldShowConnecting = () => {
   const isInternetReachable = useAppStore((s) => s.isInternetReachable);
   const { localClientConnected, webviewClientConnected, reconnecting } =
     useChatStore((s) =>
