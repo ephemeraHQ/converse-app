a6484b49ed0013badf5bb5e36b68cbc2d5064c80
nmalzieu <noe.malzieu@gmail.com>
2023-09-20 16:09:18 +0200

    When using movefile, replace it!

---
+++ utils/fileSystem.ts
@@ -0,0 +1,19 @@
+import RNFS from "react-native-fs";
+
+export const moveFileAndReplace = async (
+  filePath: string,
+  destPath: string
+) => {
+  const [filePathExists, destPathExists] = await Promise.all([
+    RNFS.exists(filePath),
+    RNFS.exists(destPath),
+  ]);
+  if (!filePathExists) {
+    throw new Error(`File ${filePath} cannot be moved as it does not exist`);
+  }
+  if (destPathExists) {
+    console.warn(`Trying to move file that already exists: ${destPath}`);
+    await RNFS.unlink(destPath);
+  }
+  await RNFS.moveFile(filePath, destPath);
+};
