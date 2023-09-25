e0720abf59a265170bafc9410f72dddfb258f463
nmalzieu <noe.malzieu@gmail.com>
2023-09-25 19:08:38 +0200

    Upgrade expo & RN

--- utils/sentry.ts
+++ utils/sentry.ts
@@ -1,6 +1,7 @@
 import * as Sentry from "sentry-expo";
 
 export const sentryTrackMessage = (message: string, extras = {} as any) => {
+  console.log(`[Sentry] ${message}`, extras);
   Sentry.React.withScope((scope) => {
     scope.setExtras(extras);
     Sentry.React.captureMessage(message);
@@ -8,6 +9,7 @@ export const sentryTrackMessage = (message: string, extras = {} as any) => {
 };
 
 export const sentryTrackError = (error: any, extras = {} as any) => {
+  console.error(error, extras);
   Sentry.React.withScope((scope) => {
     scope.setExtras(extras);
     Sentry.React.captureException(error);
