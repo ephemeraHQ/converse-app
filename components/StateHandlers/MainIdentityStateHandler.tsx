ef519066be1089b96cddf2b0ace3ce35bde12dad
nmalzieu <noe.malzieu@gmail.com>
2023-09-26 18:12:38 +0200

    Fix race condition for saveUser

--- components/StateHandlers/MainIdentityStateHandler.tsx
+++ components/StateHandlers/MainIdentityStateHandler.tsx
@@ -2,12 +2,14 @@ import { useEffect } from "react";
 
 import { refreshProfileForAddress } from "../../data/helpers/profiles/profilesUpdate";
 import { useUserStore } from "../../data/store/accountsStore";
+import { saveUser } from "../../utils/api";
 
 export default function MainIdentityStateHandler() {
   const userAddress = useUserStore((s) => s.userAddress);
 
   useEffect(() => {
     if (userAddress) {
+      saveUser(userAddress);
       refreshProfileForAddress(userAddress, userAddress);
     }
   }, [userAddress]);
