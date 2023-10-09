7ead75aa6546de263a6c074f4799156a21f37436
Noé Malzieu <noe.malzieu@gmail.com>
2023-10-09 12:25:59 +0200

    Cleanup

--- screens/ConversationList.tsx
+++ screens/ConversationList.tsx
@@ -32,9 +32,6 @@ import {
   textPrimaryColor,
   backgroundColor,
   itemSeparatorColor,
-  listItemSeparatorColor,
-  textSecondaryColor,
-  primaryColor,
 } from "../utils/colors";
 import {
   LastMessagePreview,
@@ -233,53 +230,6 @@ const useStyles = () => {
     },
     scrollViewWrapper: {
       backgroundColor: backgroundColor(colorScheme),
-    },
-    requestsHeader: {
-      flexDirection: "row",
-      alignItems: "center",
-      ...Platform.select({
-        default: {
-          paddingVertical: 8,
-          paddingRight: 24,
-          marginLeft: 32,
-          borderBottomWidth: 0.25,
-          borderBottomColor: listItemSeparatorColor(colorScheme),
-        },
-        android: {
-          paddingVertical: 12,
-          paddingHorizontal: 16,
-        },
-      }),
-    },
-    requestsHeaderTitle: {
-      color: textPrimaryColor(colorScheme),
-      ...Platform.select({
-        default: {
-          fontSize: 17,
-          fontWeight: "600",
-          marginBottom: 3,
-          marginRight: 110,
-        },
-        android: {
-          fontSize: 16,
-        },
-      }),
-    },
-    requestsCount: {
-      marginLeft: "auto",
-
-      ...Platform.select({
-        default: {
-          marginRight: 16,
-          fontSize: 15,
-          color: textSecondaryColor(colorScheme),
-        },
-        android: {
-          marginRight: 1,
-          fontSize: 11,
-          color: primaryColor(colorScheme),
-        },
-      }),
     },
   });
 };
