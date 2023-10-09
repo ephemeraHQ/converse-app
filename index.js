06c1c30de60065dd39ccbb8a101e3915a1dc3847
Noé Malzieu <noe.malzieu@gmail.com>
2023-10-09 12:25:59 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
-import { LogBox } from 'react-native';
 
 import App from "./App";
-
-LogBox.ignoreLogs([
-  'Non-serializable values were found in the navigation state',
-]);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
