ebe7e98ea958d67b40e38d7715e8ad728d9c0ff9
Noé Malzieu <noe.malzieu@gmail.com>
2023-10-20 10:43:10 +0200

    Enable null databaseId

--- android/app/src/main/java/com/converse/dev/Datatypes.kt
+++ android/app/src/main/java/com/converse/dev/Datatypes.kt
@@ -5,5 +5,5 @@ class ConversationDictData(val shortAddress: String? = null, val title: String? 
 class SavedNotificationMessage(val topic: String, val content: String, val senderAddress: String, val sent: Long, val id: String, val sentViaConverse: Boolean, val contentType: String, val account: String? = null)
 class ConversationContext(val conversationId: String, val metadata: Map<String, Any>)
 class SavedNotificationConversation(val topic: String, val peerAddress: String, val createdAt: Long, val context: ConversationContext?, val account: String? = null)
-class Accounts(val currentAccount: String, val accounts: Array<String>, val databaseId: Map<String, String>?)
+class Accounts(val currentAccount: String, val accounts: Array<String>, val databaseId: Map<String, String>? = null)
 class AccountsStore(val state: Accounts, val version: Int)
