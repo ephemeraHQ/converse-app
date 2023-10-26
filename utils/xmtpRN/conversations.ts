f8ae47f35c8e3915280c06b010c107eb652b30d8
Louis <dev@louis.io>
2023-10-26 19:03:12 +0200

    WIP

--- utils/xmtpRN/conversations.ts
+++ utils/xmtpRN/conversations.ts
@@ -37,6 +37,7 @@ const protocolConversationToStateConversation = (
   readUntil: 0,
   pending: false,
   version: conversation.version,
+  // here spamScore should be set, however it's not available in XMTP Conversation
 });
 
 const protocolConversationsToTopicData = async (
