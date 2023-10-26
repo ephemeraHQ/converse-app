e3f78a864ffb4fe144b28b3bd27db969b642236f
Louis <dev@louis.io>
2023-10-26 16:01:48 +0200

    Kotlin: save spamScore to mmkv

--- android/app/src/main/java/com/converse/dev/Datatypes.kt
+++ android/app/src/main/java/com/converse/dev/Datatypes.kt
@@ -8,7 +8,7 @@ class SavedNotificationMessage(val topic: String, val content: String, val sende
 class ConversationContext(val conversationId: String, val metadata: Map<String, Any>)
 class Accounts(val currentAccount: String, val accounts: Array<String>, val databaseId: Map<String, String>? = null)
 class AccountsStore(val state: Accounts, val version: Int)
-class SavedNotificationConversation(val topic: String, val peerAddress: String, val createdAt: Long, val context: ConversationContext?, val account: String? = null) {
+class SavedNotificationConversation(val topic: String, val peerAddress: String, val createdAt: Long, val context: ConversationContext?, val account: String? = null, spamScore: Double? = null) {
   // Whenever spamScore is set, round it to two decimal to ensure it fits as a 2-digit float in the database
   var spamScore: Double? = null
     set(value) {
