3b35fbf7908579cffdde31e826357d23098a0edd
Louis <dev@louis.io>
2023-11-16 12:17:46 +0100

    Update `getReadableProfile` and `useAccountsProfiles` to display profiles with this priority order: UserName, ENS, Unstoppable Domain, and 0x

--- utils/str.ts
+++ utils/str.ts
@@ -34,10 +34,17 @@ export const getTitleFontScale = (): number => {
 export type TextInputWithValue = TextInput & { currentValue: string };
 
 export const getReadableProfile = (account: string, address: string) => {
+  const primaryUserName = getProfilesStore(account)
+    .getState()
+    .profiles[address]?.socials.userNames?.find((e) => e.isPrimary)?.name;
   const primaryENS = getProfilesStore(account)
     .getState()
     .profiles[address]?.socials.ensNames?.find((e) => e.isPrimary)?.name;
-  return primaryENS || shortAddress(account);
+  const primaryUns = getProfilesStore(account)
+    .getState()
+    .profiles[address]?.socials.unstoppableDomains?.find((e) => e.isPrimary)
+    ?.domain;
+  return primaryUserName || primaryENS || primaryUns || shortAddress(account);
 };
 
 export const useAccountsProfiles = () => {
@@ -48,14 +55,26 @@ export const useAccountsProfiles = () => {
 
   const handleAccount = useCallback(
     (account: string, state: ProfilesStoreType) => {
+      const primaryUserName = state.profiles[account]?.socials.userNames?.find(
+        (e) => e.isPrimary
+      )?.name;
       const primaryENS = state.profiles[account]?.socials.ensNames?.find(
         (e) => e.isPrimary
       )?.name;
-      const readableProfile = primaryENS || shortAddress(account);
+      const primaryUns = state.profiles[
+        account
+      ]?.socials.unstoppableDomains?.find((e) => e.isPrimary)?.domain;
+
+      const readableProfile =
+        primaryUserName || primaryENS || primaryUns || shortAddress(account);
       if (accountsProfiles[account] !== readableProfile) {
         setAccountsProfiles((s) => ({
           ...s,
-          [account]: primaryENS || shortAddress(account),
+          [account]:
+            primaryUserName ||
+            primaryENS ||
+            primaryUns ||
+            shortAddress(account),
         }));
       }
     },
