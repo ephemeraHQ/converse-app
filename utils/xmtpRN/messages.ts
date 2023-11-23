9ce135ac64bf0b89e8cc93722b593bd65ab0defe
Noé Malzieu <noe.malzieu@gmail.com>
2023-11-23 12:09:16 +0100

    More logs for daria android

--- utils/xmtpRN/messages.ts
+++ utils/xmtpRN/messages.ts
@@ -7,6 +7,7 @@ import {
 import { addLog } from "../../components/DebugButton";
 import { saveMessages } from "../../data/helpers/messages";
 import { XmtpMessage } from "../../data/store/chatStore";
+import { sentryTrackError } from "../sentry";
 
 const BATCH_QUERY_PAGE_SIZE = 30;
 
@@ -75,71 +76,81 @@ export const loadConversationsMessages = async (
   client: Client,
   _queryConversationsFromTimestamp: { [topic: string]: number }
 ): Promise<number> => {
-  const queryConversationsFromTimestamp = {
-    ..._queryConversationsFromTimestamp,
-  };
-  let messagesFetched = 0;
-
-  while (Object.keys(queryConversationsFromTimestamp).length > 0) {
-    const topicsToQuery = Object.keys(queryConversationsFromTimestamp);
-    const messagesBatch = await client.listBatchMessages(
-      topicsToQuery.map((topic) => ({
-        contentTopic: topic,
-        startTime: new Date(queryConversationsFromTimestamp[topic]),
-        pageSize: BATCH_QUERY_PAGE_SIZE,
-        direction: "SORT_DIRECTION_ASCENDING",
-      }))
-    );
-    console.log(
-      `[XmtpRn] Fetched ${messagesBatch.length} messages from network for ${client.address}`
-    );
-
-    const oldQueryConversationsFromTimestamp = {
-      ...queryConversationsFromTimestamp,
+  try {
+    const queryConversationsFromTimestamp = {
+      ..._queryConversationsFromTimestamp,
     };
+    let messagesFetched = 0;
 
-    const messagesByTopic: { [topic: string]: DecodedMessage[] } = {};
-    messagesBatch.forEach((m) => {
-      messagesByTopic[m.topic] = messagesByTopic[m.topic] || [];
-      messagesByTopic[m.topic].push(m);
-      if (m.sent > queryConversationsFromTimestamp[m.topic]) {
-        queryConversationsFromTimestamp[m.topic] = m.sent;
-      }
-    });
+    while (Object.keys(queryConversationsFromTimestamp).length > 0) {
+      const topicsToQuery = Object.keys(queryConversationsFromTimestamp);
+      addLog(`4. Loading messages for ${topicsToQuery.length} conversations`);
+      const messagesBatch = await client.listBatchMessages(
+        topicsToQuery.map((topic) => ({
+          contentTopic: topic,
+          startTime: new Date(queryConversationsFromTimestamp[topic]),
+          pageSize: BATCH_QUERY_PAGE_SIZE,
+          direction: "SORT_DIRECTION_ASCENDING",
+        }))
+      );
+      addLog(`5. Fetched ${messagesBatch.length} messages from network`);
 
-    topicsToQuery.forEach((topic) => {
-      const messages = messagesByTopic[topic];
-      if (!messages || messages.length <= 1) {
-        // When we have no more messages for a topic it means we have gone through all of it
-        // Checking if messages.length < BATCH_QUERY_PAGE_SIZE would be more performant (one less query
-        // per topic) but could miss messages because if there are messages that are not decoded they
-        // are not returned by listBatchMessages)
-        delete queryConversationsFromTimestamp[topic];
-      }
-    });
+      console.log(
+        `[XmtpRn] Fetched ${messagesBatch.length} messages from network for ${client.address}`
+      );
 
-    // To avoid a loop let's verify that we don't query a topic
-    // again with the exact same timestamp
-    Object.keys(queryConversationsFromTimestamp).forEach((topic) => {
-      if (
-        queryConversationsFromTimestamp[topic] ===
-        oldQueryConversationsFromTimestamp[topic]
-      ) {
-        console.log(
-          "[XmtpRn] Avoiding a loop during sync due to weird timestamps"
-        );
-        addLog(`Avoiding a loop`);
-        queryConversationsFromTimestamp[topic] += 1;
-      }
-    });
+      const oldQueryConversationsFromTimestamp = {
+        ...queryConversationsFromTimestamp,
+      };
 
-    messagesBatch.sort((messageA, messageB) => messageA.sent - messageB.sent);
-    messagesFetched += messagesBatch.length;
-    saveMessages(
-      client.address,
-      messagesBatch.map(protocolMessageToStateMessage)
-    );
+      const messagesByTopic: { [topic: string]: DecodedMessage[] } = {};
+      messagesBatch.forEach((m) => {
+        messagesByTopic[m.topic] = messagesByTopic[m.topic] || [];
+        messagesByTopic[m.topic].push(m);
+        if (m.sent > queryConversationsFromTimestamp[m.topic]) {
+          queryConversationsFromTimestamp[m.topic] = m.sent;
+        }
+      });
+
+      topicsToQuery.forEach((topic) => {
+        const messages = messagesByTopic[topic];
+        if (!messages || messages.length <= 1) {
+          // When we have no more messages for a topic it means we have gone through all of it
+          // Checking if messages.length < BATCH_QUERY_PAGE_SIZE would be more performant (one less query
+          // per topic) but could miss messages because if there are messages that are not decoded they
+          // are not returned by listBatchMessages)
+          delete queryConversationsFromTimestamp[topic];
+        }
+      });
+
+      // To avoid a loop let's verify that we don't query a topic
+      // again with the exact same timestamp
+      Object.keys(queryConversationsFromTimestamp).forEach((topic) => {
+        if (
+          queryConversationsFromTimestamp[topic] ===
+          oldQueryConversationsFromTimestamp[topic]
+        ) {
+          console.log(
+            "[XmtpRn] Avoiding a loop during sync due to weird timestamps"
+          );
+          addLog(`6. Avoiding a loop`);
+          queryConversationsFromTimestamp[topic] += 1;
+        }
+      });
+
+      messagesBatch.sort((messageA, messageB) => messageA.sent - messageB.sent);
+      messagesFetched += messagesBatch.length;
+      addLog(`7. Saving ${messagesBatch.length} messages`);
+      saveMessages(
+        client.address,
+        messagesBatch.map(protocolMessageToStateMessage)
+      );
+    }
+    addLog(`8. Fetched ${messagesFetched} messages from network`);
+    return messagesFetched;
+  } catch (e) {
+    addLog(`9. Got an error while loading messages : ${e.toString()}`);
+    sentryTrackError(e);
+    throw e;
   }
-  addLog(`Fetched ${messagesFetched} messages from network`);
-  return messagesFetched;
 };
