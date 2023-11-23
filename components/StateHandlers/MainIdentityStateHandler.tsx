73c54e8fcb0f7647c90364de1d1e4c3753a361bd
Louis <dev@louis.io>
2023-11-23 12:19:12 +0100

    Revert `handleIdentityState()` and remove logs

--- components/StateHandlers/MainIdentityStateHandler.tsx
+++ components/StateHandlers/MainIdentityStateHandler.tsx
@@ -1,31 +1,18 @@
 import { useEffect } from "react";
 
 import { refreshProfileForAddress } from "../../data/helpers/profiles/profilesUpdate";
-import {
-  useCurrentAccount,
-  useAccountsStore,
-} from "../../data/store/accountsStore";
+import { useCurrentAccount } from "../../data/store/accountsStore";
 import { saveUser } from "../../utils/api";
 
 export default function MainIdentityStateHandler() {
   const userAddress = useCurrentAccount();
-  const privyAccountId = useAccountsStore((s) => s.privyAccountId);
 
   useEffect(() => {
-    const handleIdentityState = async () => {
-      console.log("## calling handleIdentityState");
-
-      if (userAddress) {
-        await saveUser(userAddress, privyAccountId[userAddress]);
-        console.log(`** [Identity] Saved user ${userAddress}`);
-
-        await refreshProfileForAddress(userAddress, userAddress);
-        console.log(`** [Identity] Refreshed profile for ${userAddress}`);
-      }
-    };
-
-    handleIdentityState();
-  }, [userAddress, privyAccountId]);
+    if (userAddress) {
+      saveUser(userAddress);
+      refreshProfileForAddress(userAddress, userAddress);
+    }
+  }, [userAddress]);
 
   return null;
 }
