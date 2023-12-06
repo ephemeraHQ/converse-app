83c249a7932d5c62e3a84bc965e9ed2192e6e8ad
Louis <dev@louis.io>
2023-12-06 18:15:34 +0100

    WIP async update for account, and await conversationConsentState()

--- data/updates/001-setConsent.ts
+++ data/updates/001-setConsent.ts
@@ -1,3 +1,32 @@
-export const setConsent = async () => {
-  console.log("[Async Updates] Running 001-setConsent");
+import { conversationConsentState } from "@xmtp/react-native-sdk";
+
+import { getChatStore } from "../../data/store/accountsStore";
+
+const logConsentStatesForAccount = async (account: string) => {
+  const conversations = getChatStore(account).getState().conversations;
+
+  for (const topic in conversations) {
+    if (conversations.hasOwnProperty(topic)) {
+      const conversation = conversations[topic];
+      try {
+        const consentState = await conversationConsentState(
+          account,
+          conversation.topic
+        );
+        console.log(
+          `Topic: ${topic}, Consent State: ${JSON.stringify(consentState)}`
+        );
+      } catch (error) {
+        console.error(
+          `Error fetching consent state for topic: ${topic}`,
+          error
+        );
+      }
+    }
+  }
+};
+
+export const setConsent = async (account: string) => {
+  console.log("[Async Updates] Running 001-setConsent, account:", account);
+  logConsentStatesForAccount(account);
 };
