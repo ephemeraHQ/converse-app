8751bc9733ce244f729a8800d7cddba28e84315f
Louis <dev@louis.io>
2023-12-11 12:52:05 +0100

    Profile search UI

--- components/Search/ProfileSearchItem.tsx
+++ components/Search/ProfileSearchItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigationState } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Platform, StyleSheet, Text, View, useColorScheme } from "react-native";
 
@@ -9,6 +10,7 @@ import {
 } from "../../utils/colors";
 import { getPreferredName, getPrimaryNames } from "../../utils/profile";
 import { shortAddress } from "../../utils/str";
+import Button from "../Button/Button";
 
 export function ProfileSearchItem({
   address,
@@ -27,24 +29,43 @@ export function ProfileSearchItem({
   const primaryNamesDisplay = primaryNames.filter(
     (name) => name !== preferredName
   );
-
-  const textAlign = "left";
+  const navigationIndex = useNavigationState((state) => state.index);
 
   return (
     <View key={address} style={[styles.recommendationBorderBottom]}>
       <View style={styles.recommendationLeft}>
-        <Text style={[styles.recommendationTitle, { textAlign }]}>
+        <Text style={[styles.recommendationTitle]}>
           {preferredName || shortAddress(address)}
         </Text>
-
         {primaryNamesDisplay.length > 0 && (
-          <Text
-            style={[styles.recommendationText, { textAlign, width: undefined }]}
-          >
+          <Text style={[styles.recommendationText]}>
             {primaryNamesDisplay.join(" | ")}
           </Text>
         )}
       </View>
+      {navigation && (
+        <View style={styles.recommendationRight}>
+          <Button
+            variant={Platform.OS === "android" ? "text" : "secondary"}
+            picto="message"
+            title="Chat"
+            style={styles.cta}
+            onPress={() => {
+              // On Android the accounts are not in the navigation but in a drawer
+              navigation.pop(
+                Platform.OS === "ios" ? navigationIndex - 1 : navigationIndex
+              );
+              setTimeout(() => {
+                console.log("open main convo with:", address);
+                navigation.navigate("Conversation", {
+                  mainConversationWithPeer: address,
+                  focus: true,
+                });
+              }, 300);
+            }}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -52,9 +73,41 @@ export function ProfileSearchItem({
 const useStyles = () => {
   const colorScheme = useColorScheme();
   return StyleSheet.create({
+    // ... other styles ...
+    recommendationBorderBottom: {
+      borderBottomWidth: 0.5,
+      borderBottomColor: itemSeparatorColor(colorScheme),
+      paddingVertical: 16, // Adjust vertical padding for each cell
+      flexDirection: "row", // Align items in a row
+      justifyContent: "space-between", // This will place items on each end
+      alignItems: "center", // Align items vertically
+    },
+    recommendationLeft: {
+      flex: 1, // Take up all available space
+      justifyContent: "center", // Center content vertically
+    },
+    recommendationRight: {
+      justifyContent: "center",
+    },
+    recommendationTitle: {
+      fontSize: 17,
+      fontWeight: "600",
+      color: textPrimaryColor(colorScheme),
+      marginBottom: 3,
+    },
+    recommendationText: {
+      fontSize: 15,
+      color: textSecondaryColor(colorScheme),
+      alignSelf: "flex-start",
+    },
+    cta: {
+      paddingHorizontal: 5, // Add padding around the button
+    },
+    // ... other styles ...
+
     recommendation: {
       flexDirection: "row",
-      paddingRight: 10,
+      paddingRight: 5,
       ...Platform.select({
         default: {
           paddingVertical: 15,
@@ -62,62 +115,11 @@ const useStyles = () => {
         android: { paddingVertical: 12 },
       }),
     },
-    recommendationBorderBottom: {
-      ...Platform.select({
-        default: {
-          borderBottomWidth: 0.5,
-          borderBottomColor: itemSeparatorColor(colorScheme),
-        },
-        android: {},
-      }),
-    },
-    recommendationLeft: {
-      flexGrow: 1,
-      flexShrink: 1,
-    },
-    recommendationRight: {
-      justifyContent: "center",
-    },
-    recommendationTitle: {
-      width: "100%",
-      color: textPrimaryColor(colorScheme),
-      ...Platform.select({
-        default: {
-          fontSize: 17,
-          fontWeight: "600",
-          marginBottom: 3,
-          marginRight: 110,
-        },
-        android: {
-          fontSize: 16,
-        },
-      }),
-    },
-    recommendationText: {
-      color: textSecondaryColor(colorScheme),
-      ...Platform.select({
-        default: {
-          fontSize: 15,
-        },
-        android: {
-          fontSize: 14,
-        },
-      }),
-      alignSelf: "flex-start",
-    },
+
     recommendationRow: {
       flexDirection: "row",
       alignItems: "center",
       marginVertical: 3,
-    },
-    recommendationImage: {
-      width: 15,
-      height: 15,
-      marginRight: 10,
-    },
-    cta: {
-      marginRight: 0,
-      marginLeft: "auto",
     },
   });
 };
