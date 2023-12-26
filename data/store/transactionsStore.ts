93dcec759887d806d6fca46e7114c3f2cab0ca9e
Louis <dev@louis.io>
2023-12-26 12:33:00 +0100

    Update accountsStore: add useTransactionsStore and getTransactionsStore, manage the deletion of store

--- data/store/transactionsStore.ts
+++ data/store/transactionsStore.ts
@@ -20,34 +20,56 @@ type Transaction = {
   events?: string;
 };
 
-type TransactionsStoreType = {
+export type TransactionsStoreType = {
   transactions: { [id: string]: Transaction };
+
   addTransaction: (transaction: Transaction) => void;
   updateTransaction: (id: string, updates: Partial<Transaction>) => void;
+
+  getEvents: (id: string) => any[];
+  setEvents: (id: string, events: any[]) => void;
 };
 
 export const initTransactionsStore = (account: string) => {
   const transactionsStore = create<TransactionsStoreType>()(
     persist(
-      (set) => ({
-        transactions: {},
+      (set, get) =>
+        ({
+          transactions: {},
 
-        addTransaction: (transaction) =>
-          set((state) => ({
-            transactions: {
-              ...state.transactions,
-              [transaction.id]: transaction,
-            },
-          })),
+          addTransaction: (transaction) =>
+            set((state) => ({
+              transactions: {
+                ...state.transactions,
+                [transaction.id]: transaction,
+              },
+            })),
 
-        updateTransaction: (id, updates) =>
-          set((state) => ({
-            transactions: {
-              ...state.transactions,
-              [id]: { ...state.transactions[id], ...updates },
-            },
-          })),
-      }),
+          updateTransaction: (id, updates) =>
+            set((state) => ({
+              transactions: {
+                ...state.transactions,
+                [id]: { ...state.transactions[id], ...updates },
+              },
+            })),
+
+          getEvents: (id) => {
+            const transaction = get().transactions[id];
+            const eventsString = transaction?.events ?? "[]";
+            return JSON.parse(eventsString);
+          },
+
+          setEvents: (id, events) =>
+            set((state) => ({
+              transactions: {
+                ...state.transactions,
+                [id]: {
+                  ...state.transactions[id],
+                  events: JSON.stringify(events),
+                },
+              },
+            })),
+        }) as TransactionsStoreType,
       {
         name: `store-${account}-transactions`, // Account-based storage so each account can have its own transactions
         storage: createJSONStorage(() => zustandMMKVStorage),
