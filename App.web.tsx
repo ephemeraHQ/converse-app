749c0dff73dd8ca3d6a7b4e728dd98deb387530c
Noé Malzieu <noe.malzieu@gmail.com>
2024-01-09 15:35:03 +0100

    Add App.web.tsx

---
+++ App.web.tsx
@@ -0,0 +1,10 @@
+import "@expo/metro-runtime";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>COUCOU WEB</Text>
+    </View>
+  );
+}
