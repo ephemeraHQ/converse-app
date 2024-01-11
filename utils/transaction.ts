79dd84731a1fc2776afe04ea621934000c1207ce
Louis <dev@louis.io>
2024-01-11 16:39:03 +0100

    Check for valid transaction reference

--- utils/transaction.ts
+++ utils/transaction.ts
@@ -27,3 +27,29 @@ export const mergeTransactionRefData = (
     events: details.events || [],
   };
 };
+
+export const isTransactionRefValid = (messageContent: string): boolean => {
+  let parsedContent;
+
+  // Try to parse the JSON content
+  try {
+    parsedContent = JSON.parse(messageContent);
+  } catch (error) {
+    console.error("Failed to parse JSON:", error);
+    return false;
+  }
+
+  // Check if the mandatory fields 'networkId' and 'reference' are present and of correct types
+  if (
+    typeof parsedContent.networkId !== "string" &&
+    typeof parsedContent.networkId !== "number"
+  ) {
+    return false;
+  }
+
+  if (typeof parsedContent.reference !== "string") {
+    return false;
+  }
+
+  return true;
+};
