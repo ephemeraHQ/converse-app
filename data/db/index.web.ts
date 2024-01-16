0420a465f4a4445e3847ceff05cc0d6c58182e75
Noé Malzieu <noe.malzieu@gmail.com>
2024-01-16 12:15:17 +0100

    Fix logout & accounts on Web

--- data/db/index.web.ts
+++ data/db/index.web.ts
@@ -12,3 +12,5 @@ const dumbRepository = {
 export const getRepository = async (account: string, entity: string) => {
   return dumbRepository;
 };
+
+export const clearDb = async (account: string) => {};
