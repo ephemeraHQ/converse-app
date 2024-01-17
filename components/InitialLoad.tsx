338cf0b5ad30e7ba905bcb2a39fd3e2f80e4ce17
Noé Malzieu <noe.malzieu@gmail.com>
2024-01-17 14:28:53 +0100

    Empty

--- components/InitialLoad.tsx
+++ components/InitialLoad.tsx
@@ -39,6 +39,7 @@ const useStyles = () => {
       flex: 1,
       justifyContent: "center",
       backgroundColor: backgroundColor(colorScheme),
+      paddingTop: Platform.OS === "web" ? 50 : undefined,
     },
     welcome: {
       marginTop: 15,
