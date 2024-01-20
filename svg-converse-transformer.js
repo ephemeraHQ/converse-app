624ff3446923f81766d0e3e22a1ce7e54c1e49de
Noé Malzieu <noe.malzieu@gmail.com>
2024-01-20 15:59:21 +0100

    sentry gradle plugin version, local transformer

---
+++ svg-converse-transformer.js
@@ -0,0 +1,44 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const upstreamTransformer = require("@expo/metro-config/babel-transformer");
+const { resolveConfig, transform } = require("@svgr/core");
+const resolveConfigDir = require("path-dirname");
+
+const defaultSVGRConfig = {
+  native: true,
+  plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+  svgoConfig: {
+    plugins: [
+      {
+        name: "preset-default",
+        params: {
+          overrides: {
+            inlineStyles: {
+              onlyMatchedOnce: false,
+            },
+            removeViewBox: false,
+            removeUnknownsAndDefaults: false,
+            convertColors: false,
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = {
+  transform: async ({ src, filename, options }) => {
+    console.log("trasnform on", filename);
+    if (filename.endsWith(".svg")) {
+      const config = await resolveConfig(resolveConfigDir(filename));
+      const svgrConfig = config
+        ? { ...defaultSVGRConfig, ...config }
+        : defaultSVGRConfig;
+      return upstreamTransformer.transform({
+        src: await transform(src, svgrConfig),
+        filename,
+        options,
+      });
+    }
+    return upstreamTransformer.transform({ src, filename, options });
+  },
+};
