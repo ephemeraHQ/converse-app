71c3880d9f3bae6df19779a30780a17f3b4db7ce
Noé Malzieu <noe.malzieu@gmail.com>
2024-01-20 15:59:21 +0100

    sentry gradle plugin version, local transformer

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,8 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const upstreamTransformer = require("@expo/metro-config/babel-transformer");
 const { mergeConfig } = require("@react-native/metro-config");
 const {
   createSentryMetroSerializer,
 } = require("@sentry/react-native/dist/js/tools/sentryMetroSerializer");
-const { resolveConfig, transform } = require("@svgr/core");
 const { getDefaultConfig } = require("expo/metro-config");
-const resolveConfigDir = require("path-dirname");
 
 const sentryConfig = {
   serializer: {
@@ -19,29 +15,6 @@ const defaultConfig = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 });
-
-
-const defaultSVGRConfig = {
-  native: true,
-  plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
-  svgoConfig: {
-    plugins: [
-      {
-        name: "preset-default",
-        params: {
-          overrides: {
-            inlineStyles: {
-              onlyMatchedOnce: false,
-            },
-            removeViewBox: false,
-            removeUnknownsAndDefaults: false,
-            convertColors: false,
-          },
-        },
-      },
-    ],
-  },
-};
 
 const converseMetroConfig = {
   ...defaultConfig,
@@ -57,22 +30,7 @@ const converseMetroConfig = {
     minifierConfig: {
       // Terser options...
     },
-    babelTransformerPath: {
-      transform: async ({ src, filename, options }) => {
-        if (filename.endsWith(".svg")) {
-          const config = await resolveConfig(resolveConfigDir(filename));
-          const svgrConfig = config
-            ? { ...defaultSVGRConfig, ...config }
-            : defaultSVGRConfig;
-          return upstreamTransformer.transform({
-            src: await transform(src, svgrConfig),
-            filename,
-            options,
-          });
-        }
-        return upstreamTransformer.transform({ src, filename, options });
-      },
-    },
+    babelTransformerPath: "./svg-converse-transformer.js",
   },
   resolver: {
     ...defaultConfig.resolver,
