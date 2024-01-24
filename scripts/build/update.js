1b48d8343d039385a8706d5958735095a7919204
Noé Malzieu <noe.malzieu@gmail.com>
2024-01-24 10:07:08 +0100

    Update script

--- scripts/build/update.js
+++ scripts/build/update.js
@@ -1,6 +1,9 @@
-const { execSync } = require("child_process");
+const { spawn } = require("child_process");
 const isClean = require("git-is-clean");
 const prompts = require("prompts");
+
+// eslint-disable-next-line no-undef
+const PROJECT_ROOT = path.join(__dirname, "..", "..");
 
 const update = async () => {
   const clean = await isClean();
@@ -19,9 +22,31 @@ const update = async () => {
   const { env } = await prompts(questions);
   if (!env) process.exit(1);
 
-  execSync(`node scripts/build/ios/${env}.js`);
-  execSync(`node scripts/build/android/${env}.js`);
-  execSync(`eas update --branch ${env}`);
+  await executeCommand("node", [`scripts/build/ios/${env}.js`]);
+  await executeCommand("node", [`scripts/build/android/${env}.js`]);
+  await executeCommand("eas", ["update", "--branch", env]);
 };
 
 update();
+
+const executeCommand = (command, args, env) =>
+  new Promise((resolve, reject) => {
+    console.log(`${command} ${args.join(" ")}`);
+    const buildProcess = spawn(command, args, {
+      stdio: "inherit",
+      cwd: PROJECT_ROOT,
+      env,
+    });
+
+    buildProcess.on("error", (error) => {
+      console.error(`Error: ${error}`);
+    });
+
+    buildProcess.on("exit", (code) => {
+      if (code === 0) {
+        return resolve();
+      }
+
+      reject(code);
+    });
+  });
