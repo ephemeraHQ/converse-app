1495890c012ae8fc29cd4db4437f31ae320759cb
Louis <dev@louis.io>
2024-02-15 14:36:37 +0100

    Add the ability to show full, non-truncated, user name in `getReadableProfile()`

--- utils/str.ts
+++ utils/str.ts
@@ -57,7 +57,11 @@ export const getTitleFontScale = (): number => {
 
 export type TextInputWithValue = TextInput & { currentValue: string };
 
-export const getReadableProfile = (account: string, address: string) => {
+export const getReadableProfile = (
+  account: string,
+  address: string,
+  showFullUsername: boolean = false
+) => {
   const primaryUserName = getProfilesStore(account)
     .getState()
     .profiles[address]?.socials.userNames?.find((e) => e.isPrimary)?.name;
@@ -69,7 +73,7 @@ export const getReadableProfile = (account: string, address: string) => {
     .profiles[address]?.socials.unstoppableDomains?.find((e) => e.isPrimary)
     ?.domain;
   return (
-    shortDomain(primaryUserName) ||
+    (showFullUsername ? primaryUserName : shortDomain(primaryUserName)) ||
     primaryENS ||
     primaryUns ||
     shortAddress(account)
