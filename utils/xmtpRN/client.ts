4c10dd1494d134eead1ed7362fced17416bdc9bc
Noé Malzieu <noe.malzieu@gmail.com>
2024-05-12 14:25:43 +0200

    Handling ongoing group messages from notification extension

--- utils/xmtpRN/client.ts
+++ utils/xmtpRN/client.ts
@@ -1,4 +1,5 @@
 import { TransactionReferenceCodec } from "@xmtp/content-type-transaction-reference";
+import { privateKey } from "@xmtp/proto";
 import {
   Client,
   GroupChangeCodec,
@@ -9,6 +10,9 @@ import {
   StaticAttachmentCodec,
   TextCodec,
 } from "@xmtp/react-native-sdk";
+import { PrivateKeyBundleV1 } from "@xmtp/xmtp-js";
+import { Platform } from "react-native";
+import RNFS from "react-native-fs";
 
 import config from "../../config";
 import { getCleanAddress } from "../eth";
@@ -16,8 +20,29 @@ import { CoinbaseMessagingPaymentCodec } from "./contentTypes/coinbasePayment";
 
 const env = config.xmtpEnv as "dev" | "production" | "local";
 
-export const getXmtpClientFromBase64Key = (base64Key: string) =>
-  Client.createFromKeyBundle(base64Key, {
+const getAddressFromBase64Key = (base64Key: string) => {
+  const privateKeyBundleProto = privateKey.PrivateKeyBundle.decode(
+    Buffer.from(base64Key, "base64")
+  );
+  if (!privateKeyBundleProto.v1) throw new Error("PrivateKeyBundleV1 missing");
+  const privateKeyBundleV1 = new PrivateKeyBundleV1(privateKeyBundleProto.v1);
+  const address = privateKeyBundleV1
+    .getPublicKeyBundle()
+    .walletSignatureAddress();
+  return address;
+};
+
+export const getXmtpClientFromBase64Key = async (base64Key: string) => {
+  const address = getAddressFromBase64Key(base64Key);
+  const dbFilename = `xmtp-${config.xmtpEnv}-${address}.db3`;
+  // @todo => use helper method for dbDirectory ?
+  // URL.documentsDirectory.appendingPathComponent("xmtp-\(options?.api.env.rawValue ?? "")-\(address).db3")
+  const dbDirectory =
+    Platform.OS === "ios"
+      ? await RNFS.pathForGroup(config.appleAppGroup)
+      : `/data/data/${config.bundleId}/databases`;
+  const dbPath = `${dbDirectory}/${dbFilename}`;
+  return Client.createFromKeyBundle(base64Key, {
     env,
     codecs: [
       new TextCodec(),
@@ -31,9 +56,13 @@ export const getXmtpClientFromBase64Key = (base64Key: string) =>
       new CoinbaseMessagingPaymentCodec(),
     ],
     enableAlphaMls: true,
+    // @todo => use getDbDirectory when we're merged with other branch?
+    // discuss with XMTP the fact that we store the data inside the shared container
+    dbPath,
     // @todo => use another key!!
     dbEncryptionKey: Buffer.from(base64Key, "base64").subarray(0, 32),
   });
+};
 
 export type ConverseXmtpClientType = Awaited<
   ReturnType<typeof getXmtpClientFromBase64Key>
