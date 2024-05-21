cefd2f93d305b78c7643a116e6803d4b0c1ac60e
Noé Malzieu <noe.malzieu@gmail.com>
2024-05-21 18:20:08 +0200

    Add loop interval for modify profile

--- utils/profile.ts
+++ utils/profile.ts
@@ -49,6 +49,23 @@ export function getPreferredName(
   return shortAddress(peerAddress);
 }
 
+export function getPreferredUsername(
+  socials: ProfileSocials | undefined
+): string | undefined {
+  const userName = socials?.userNames?.find((e) => e.isPrimary) || null;
+  const ensName = socials?.ensNames?.find((e) => e.isPrimary) || null;
+  const unsDomain =
+    socials?.unstoppableDomains?.find((d) => d.isPrimary) || null;
+
+  if (userName) {
+    return userName.name;
+  } else if (ensName) {
+    return ensName.name;
+  } else if (unsDomain) {
+    return unsDomain.domain;
+  }
+}
+
 export function getPreferredAvatar(
   socials: ProfileSocials | undefined
 ): string | undefined {
