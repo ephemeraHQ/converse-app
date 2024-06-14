c914f46dea5503a147218cb28f5ad08767452ad4
Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>
2024-06-14 08:13:06 -0600

    bump latest SDK for testing

--- queries/useGroupQuery.ts
+++ queries/useGroupQuery.ts
@@ -6,7 +6,6 @@ import { useGroupsQuery } from "./useGroupsQuery";
 export const useGroupQuery = (account: string, topic: string) => {
   const { data } = useGroupsQuery(account);
   const group = data?.byId[topic] ?? null;
-  console.log("group", !!group, topic, data?.ids);
   return useQuery({
     queryKey: groupQueryKey(account, topic),
     queryFn: async () => {
