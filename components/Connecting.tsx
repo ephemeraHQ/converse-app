18b25da46c2645606e6dfef252ffd78ee671b7ab
Saul Carlin <saul.carlin@gmail.com>
2024-06-19 09:47:20 -0700

    cleanup header

--- components/Connecting.tsx
+++ components/Connecting.tsx
@@ -1,9 +1,6 @@
-import { View, Text, useColorScheme, Platform } from "react-native";
-
 import { useChatStore } from "../data/store/accountsStore";
 import { useAppStore } from "../data/store/appStore";
 import { useSelect } from "../data/store/storeHelpers";
-import { textPrimaryColor } from "../utils/colors";
 import ActivityIndicator from "./ActivityIndicator/ActivityIndicator";
 
 export const useShouldShowConnecting = () => {
@@ -26,30 +23,5 @@ export const useShouldShowConnectingOrSyncing = () => {
 };
 
 export default function Connecting() {
-  const colorScheme = useColorScheme();
-  const shouldShowConnecting = useShouldShowConnecting();
-
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        width: Platform.OS === "ios" ? 110 : 130,
-      }}
-    >
-      <ActivityIndicator />
-      <Text
-        style={{
-          marginLeft: 10,
-          color: textPrimaryColor(colorScheme),
-          ...Platform.select({
-            android: { fontSize: 22, fontFamily: "Roboto" },
-          }),
-        }}
-      >
-        {shouldShowConnecting ? "Connecting" : "Syncing"}
-      </Text>
-    </View>
-  );
+  return <ActivityIndicator />;
 }
