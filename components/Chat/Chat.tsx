3f829755c8a79359041924b931fa1d76d30b3f37
Saul Carlin <saul@xmtp.com>
2024-06-25 00:12:16 -0700

    context menu wip

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -375,13 +375,16 @@ const useStyles = () => {
       flex: 1,
       justifyContent: "flex-end",
       backgroundColor: backgroundColor(colorScheme),
+      overflow: "visible",
     },
     chatContent: {
       backgroundColor: backgroundColor(colorScheme),
       flex: 1,
+      overflow: "visible",
     },
     chat: {
       backgroundColor: backgroundColor(colorScheme),
+      overflow: "visible",
     },
     inputBottomFiller: {
       position: "absolute",
