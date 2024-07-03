ed3197bdb3a6cee6e47141d61860e58bf30f3063
Saul Carlin <saul@xmtp.com>
2024-07-03 14:47:54 -0700

    remove encrypted label for pending message

--- components/Chat/Message/MessageStatus.tsx
+++ components/Chat/Message/MessageStatus.tsx
@@ -1,6 +1,6 @@
 import { textSecondaryColor } from "@styles/colors";
-import React from "react";
-import { StyleSheet, Text, useColorScheme } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, useColorScheme, Animated } from "react-native";
 
 import { MessageToDisplay } from "./Message";
 
@@ -10,20 +10,56 @@ type Props = {
 
 export default function MessageStatus({ message }: Props) {
   const styles = useStyles();
+  const opacityAnim = useRef(new Animated.Value(0)).current;
+  const heightAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (message.status === "sent") {
+      Animated.parallel([
+        Animated.timing(opacityAnim, {
+          toValue: 1,
+          duration: 300,
+          useNativeDriver: true,
+        }),
+        Animated.timing(heightAnim, {
+          toValue: 20,
+          duration: 300,
+          useNativeDriver: false,
+        }),
+      ]).start();
+    } else {
+      Animated.parallel([
+        Animated.timing(opacityAnim, {
+          toValue: 0,
+          duration: 300,
+          useNativeDriver: true,
+        }),
+        Animated.timing(heightAnim, {
+          toValue: 0,
+          duration: 300,
+          useNativeDriver: false,
+        }),
+      ]).start();
+    }
+  }, [message.status, opacityAnim, heightAnim]);
 
   return (
-    message.fromMe &&
-    (message.status === "sending" || message.status === "prepared" ? (
-      <Text style={styles.statusText}>Encrypted</Text>
-    ) : (
-      <Text style={styles.statusText}>Sent</Text>
-    ))
+    message.fromMe && (
+      <Animated.View style={[styles.container, { height: heightAnim }]}>
+        <Animated.Text style={[styles.statusText, { opacity: opacityAnim }]}>
+          Sent
+        </Animated.Text>
+      </Animated.View>
+    )
   );
 }
 
 const useStyles = () => {
   const colorScheme = useColorScheme();
   return StyleSheet.create({
+    container: {
+      overflow: "hidden",
+    },
     statusText: {
       fontSize: 12,
       color: textSecondaryColor(colorScheme),
