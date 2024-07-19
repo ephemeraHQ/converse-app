bd2d5ddb55f89feede6aed3141ec10900e3247ca
Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>
2024-07-18 22:46:45 -0600

    feat: Send Message from Profile
    Added ability to send message from profile when user is not blocked
    
    Updated some more usages of strings

--- utils/i18n/strings.ts
+++ utils/i18n/strings.ts
@@ -1,4 +1,12 @@
 export const strings = {
+  // Onboarding
+  sign_1_of_2: "Sign (1/2)",
+  first_signature_explanation:
+    "This first signature will enable your wallet to send and receive messages.\n\n",
+  sign_2_of_2: "Sign (2/2)",
+  sign_access:
+    "Please sign one last time to access Converse and start chatting.",
+
   // Conversation List
   delete: "Delete",
   delete_and_block: "Delete and block",
@@ -11,6 +19,8 @@ export const strings = {
   decline: "Decline",
   unblock: "Unblock",
   this_group_is_blocked: "This group is blocked",
+  if_you_unblock_contact:
+    "If you unblock this contact, they will be able to send you messages again.",
   if_you_block_contact:
     "If you block this contact, you will not receive messages from them anymore",
   if_you_unblock_group:
@@ -25,4 +35,10 @@ export const strings = {
   group_name: "GROUP NAME",
   group_description: "GROUP DESCRIPTION",
   upload_group_photo_error: "Error uploading group photo",
+
+  // Profile
+  remove_from_group: "Remove from group",
+  are_you_sure: "Are you sure?",
+  promote_to_admin: "Promote to admin",
+  send_a_message: "Send a message",
 };
