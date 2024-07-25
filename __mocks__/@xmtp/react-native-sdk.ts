246d4ed4cfecd71df01fab298acb91b260fd00bc
Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>
2024-07-24 20:21:18 -0600

    More tests

---
+++ __mocks__/@xmtp/react-native-sdk.ts
@@ -0,0 +1,33 @@
+function mockedClient() {
+  return {
+    conversations: {
+      streamAllMessages: jest.fn(() => {}),
+      cancelStreamAllMessages: jest.fn(() => {}),
+      cancelStream: jest.fn(() => {}),
+    },
+    exportKeyBundle: jest.fn(() => "keybundle"),
+    canMessage: jest.fn(() => true),
+  };
+}
+
+module.exports = {
+  Client: {
+    createFromKeyBundle: jest.fn().mockImplementation(mockedClient),
+    createRandom: jest.fn().mockImplementation(mockedClient),
+  },
+  StaticAttachmentCodec: jest.fn().mockImplementation(() => {
+    return {};
+  }),
+  RemoteAttachmentCodec: jest.fn().mockImplementation(() => {
+    return {};
+  }),
+  JSContentCodec: jest.fn().mockImplementation(() => {
+    return {};
+  }),
+  GroupUpdatedCodec: jest.fn().mockImplementation(() => {
+    return {};
+  }),
+  ReplyCodec: jest.fn().mockImplementation(() => ({})),
+  ReactionCodec: jest.fn().mockImplementation(() => ({})),
+  emitter: { removeAllListeners: jest.fn(() => {}) },
+};
