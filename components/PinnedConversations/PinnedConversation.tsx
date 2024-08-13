08e9568f48567fa7782034f8c29f02cc1190d8d5
Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>
2024-08-13 10:21:06 -0600

    Update pinned conversation as well

--- components/PinnedConversations/PinnedConversation.tsx
+++ components/PinnedConversations/PinnedConversation.tsx
@@ -1,3 +1,5 @@
+import { useGroupNameQuery } from "@queries/useGroupNameQuery";
+import { useGroupPhotoQuery } from "@queries/useGroupPhotoQuery";
 import { backgroundColor, textSecondaryColor } from "@styles/colors";
 import { AvatarSizes } from "@styles/sizes";
 import { FC, useCallback } from "react";
@@ -9,10 +11,12 @@ import {
 } from "react-native";
 
 import Avatar from "..//Avatar";
-import { useChatStore, useProfilesStore } from "../../data/store/accountsStore";
+import {
+  useChatStore,
+  useCurrentAccount,
+  useProfilesStore,
+} from "../../data/store/accountsStore";
 import { XmtpConversation } from "../../data/store/chatStore";
-import { useGroupName } from "../../hooks/useGroupName";
-import { useGroupPhoto } from "../../hooks/useGroupPhoto";
 import { navigate } from "../../utils/navigation";
 import { getPreferredAvatar, getPreferredName } from "../../utils/profile";
 import GroupAvatar from "../GroupAvatar";
@@ -22,10 +26,15 @@ interface Props {
 }
 
 export const PinnedConversation: FC<Props> = ({ conversation }) => {
+  const account = useCurrentAccount() as string;
   const profiles = useProfilesStore((s) => s.profiles);
   const { topic, isGroup } = conversation;
-  const { groupName } = useGroupName(topic);
-  const { groupPhoto } = useGroupPhoto(topic);
+  const { data: groupName } = useGroupNameQuery(account, topic, {
+    refetchOnMount: false,
+  });
+  const { data: groupPhoto } = useGroupPhotoQuery(account, topic, {
+    refetchOnMount: false,
+  });
   const title = isGroup ? groupName : conversation.conversationTitle;
   const socials = profiles[conversation.peerAddress as string]?.socials;
   const avatar = isGroup ? groupPhoto : getPreferredAvatar(socials);
