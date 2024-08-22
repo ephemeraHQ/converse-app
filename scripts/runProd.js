41d5e2d659ef4df67adbd2ca3da0ee4724661a95
Noé Malzieu <noe.malzieu@gmail.com>
2024-08-22 16:50:27 +0200

    Add a local script to run a prod-like app locally

---
+++ scripts/runProd.js
@@ -0,0 +1,17 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+const runProd = async () => {
+  if (!fs.existsSync(".env.production")) {
+    throw new Error(".env.production is required to run prod locally");
+  }
+  if (fs.existsSync(".env")) {
+    throw new Error(
+      ".env should be renamed .env.development so it's not merged with .env.production values"
+    );
+  }
+
+  execSync("npx expo start --no-dev", { stdio: "inherit" });
+};
+
+runProd();
