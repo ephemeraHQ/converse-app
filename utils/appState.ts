ba0edff0b7d95c2c4a8250cca30e48266a3211a3
Noé Malzieu <noe.malzieu@gmail.com>
2024-08-22 11:51:04 +0200

    use event handler to detect app back to active

--- utils/appState.ts
+++ utils/appState.ts
@@ -8,21 +8,28 @@ helpful to make sure we're not doing critical database stuff
 while the database is disconnected to prevent data loss
 Will check every delayMs, then wait endDelayMs for db reconnection
 */
-export const waitUntilAppActive = async (
-  delayMs: number,
-  endDelayMs: number
-) => {
-  let appWasInactive = false;
-  while (AppState.currentState !== "active") {
-    appWasInactive = true;
-    logger.debug(`App is inactive, waiting ${delayMs}ms until back active`);
-    await new Promise((r) => setTimeout(r, delayMs));
-  }
-  if (appWasInactive) {
-    logger.debug(`App was inactive for a while, waiting ${endDelayMs}ms more`);
-    await new Promise((r) => setTimeout(r, endDelayMs));
-    if (AppState.currentState !== "active") {
-      await waitUntilAppActive(delayMs, endDelayMs);
-    }
-  }
+export const waitUntilAppActive = async (endDelayMs: number) => {
+  // If app is active, return immediatly
+  if (AppState.currentState === "active") return;
+  logger.debug(`Waiting until app is back into active state...`);
+  return new Promise((resolve) => {
+    const subscription = AppState.addEventListener(
+      "change",
+      async (nextAppState) => {
+        if (nextAppState === "active") {
+          subscription.remove();
+          logger.debug(
+            `App was inactive for a while, waiting ${endDelayMs}ms more`
+          );
+          await new Promise((r) => setTimeout(r, endDelayMs));
+          // Check if app is still active after endDelayMs
+          if (AppState.currentState !== "active") {
+            await waitUntilAppActive(endDelayMs);
+          } else {
+            resolve(undefined);
+          }
+        }
+      }
+    );
+  });
 };
