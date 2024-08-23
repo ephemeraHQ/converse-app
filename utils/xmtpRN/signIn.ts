10fc261a8770b513308aff24a5570287f311a56a
Noé Malzieu <noe.malzieu@gmail.com>
2024-08-23 15:13:19 +0200

    Revoke other installations when logging in (temporary feature)

--- utils/xmtpRN/signIn.ts
+++ utils/xmtpRN/signIn.ts
@@ -1,12 +1,15 @@
+import { awaitableAlert } from "@utils/alert";
 import {
   copyDatabasesToTemporaryDirectory,
   createTemporaryDirectory,
-  moveTemporaryDatabasesToDatabaseDirecory,
+  moveTemporaryDatabasesToDatabaseDirecory as moveTemporaryDatabasesToDatabaseDirectory,
 } from "@utils/fileSystem";
 import { getDbEncryptionKey } from "@utils/keychain/helpers";
+import logger from "@utils/logger";
 import { sentryAddBreadcrumb } from "@utils/sentry";
 import { Client } from "@xmtp/react-native-sdk";
 import { Signer } from "ethers";
+import { AppState } from "react-native";
 
 import config from "../../config";
 
@@ -52,10 +55,70 @@ export const getXmtpBase64KeyFromSigner = async (
 
   sentryAddBreadcrumb("Instantiated client from signer, exporting key bundle");
   const base64Key = await client.exportKeyBundle();
+
+  const isConnectingViaWallet =
+    !!preCreateIdentityCallback ||
+    !!preEnableIdentityCallback ||
+    !!preAuthenticateToInboxCallback;
+  await revokeOtherInstallations(signer, client, isConnectingViaWallet);
+
   // This Client is only be used to extract the key, we can disconnect
   // it to prevent locks happening during Onboarding
   await client.dropLocalDatabaseConnection();
-  await moveTemporaryDatabasesToDatabaseDirecory(tempDirectory, client.inboxId);
+  await moveTemporaryDatabasesToDatabaseDirectory(
+    tempDirectory,
+    client.inboxId
+  );
   sentryAddBreadcrumb("Exported key bundle");
   return base64Key;
+};
+
+/*
+Temporary method for XMTP team to revoke other installations
+when logging in to remove weird, broken installation
+*/
+const revokeOtherInstallations = async (
+  signer: Signer,
+  client: Client,
+  showAlert: boolean
+) => {
+  const state = await client.inboxState(true);
+  logger.warn(
+    `Inbox ${client.inboxId} has ${state.installationIds.length} installations`
+  );
+  const otherInstallations = state.installationIds.filter(
+    (installationId) => installationId !== client.installationId
+  );
+  if (otherInstallations.length > 0) {
+    if (showAlert) {
+      // We're on a mobile wallet so we need to ask the user first
+      await awaitableAlert(
+        `You have ${otherInstallations.length} other installations`,
+        "Temporary debugging feature: we will revoke those installations now"
+      );
+    }
+    /* On iOS, when we leave the app, it will automatically disconnect db
+    and might not reconect fast enough when coming back from that signature
+    and hit "Client error: storage error: Pool needs to  reconnect before use"
+    We should find a long term solution but in the meantime making sure we reconnect!
+    */
+    const reconnectWhenBackgrounded = AppState.addEventListener(
+      "change",
+      (state) => {
+        if (state.match(/inactive|background/)) {
+          reconnectWhenBackgrounded.remove();
+          // We don't really know when database will be disconnected so
+          // might as well try a few times...
+          client.reconnectLocalDatabase();
+          setTimeout(() => {
+            client.reconnectLocalDatabase();
+          }, 500);
+          setTimeout(() => {
+            client.reconnectLocalDatabase();
+          }, 1000);
+        }
+      }
+    );
+    await client.revokeAllOtherInstallations(signer);
+  }
 };
