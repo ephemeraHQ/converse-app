55a9e8ca9e7c1c19f405d7bf1bc493146b1d0591
Alex Risch <alex@xmtp.com>
2024-08-26 21:59:28 -0600

    feat: Remove Profile Picture Requirement when onboarding
    Removed requirement

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -119,13 +119,7 @@ export default function Main() {
           Platform.OS === "android"))
     ) {
       screenToShow = <NotificationsScreen />;
-    } else if (
-      !(
-        currentUserName?.name &&
-        currentUserName?.displayName &&
-        currentUserName?.avatar
-      )
-    ) {
+    } else if (!(currentUserName?.name && currentUserName?.displayName)) {
       screenToShow = <UserProfile onboarding />;
     } else if (Platform.OS === "android") {
       // On Android the whole navigation is wrapped in a drawler
