fca2ccd4f93d098997b9d18dae037e0b180b4603
Alex Risch <alex@xmtp.com>
2024-08-30 21:48:22 -0600

    Small updates
    Removed promise.all
    Moved to separate util file
    Added test

---
+++ utils/set.ts
@@ -0,0 +1,9 @@
+// Helper function to compare two sets
+export const areSetsEqual = (
+  setA: Set<unknown>,
+  setB: Set<unknown>
+): boolean => {
+  if (setA.size !== setB.size) return false;
+  for (const a of setA) if (!setB.has(a)) return false;
+  return true;
+};
