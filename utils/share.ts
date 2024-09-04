35c7e4273a9e582f52460bbab10bcc0724df49ae
Alex Risch <alex@xmtp.com>
2024-09-04 18:48:30 -0500

    fix: Fix Web
    Added some platform specific usage for Web
    Updated ReadMe

---
+++ utils/share.ts
@@ -0,0 +1,9 @@
+import RNShare from "react-native-share";
+
+class Share {
+  open(payload: { title: string; url: string; type: string }) {
+    RNShare.open(payload);
+  }
+}
+
+export default new Share();
