1fd276be73a2ef843b63523a2f73c15b74608115
Noé Malzieu <noe.malzieu@gmail.com>
2024-09-05 13:22:56 +0200

    Implement a key based persister for react query using mmkv

--- queries/queryClient.ts
+++ queries/queryClient.ts
@@ -1,11 +1,22 @@
 import { QueryClient } from "@tanstack/react-query";
+import { experimental_createPersister } from "@tanstack/react-query-persist-client";
+import { reactQuerySyncStorage } from "@utils/mmkv";
+
+const GC_TIME = 1000 * 60 * 60 * 24; // 24 hours
+const STALE_TIME = 1000 * 60 * 60; // 1 hour
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      gcTime: 1000 * 60 * 60 * 24, // 24 hours
-      staleTime: 1000 * 60 * 60, // 1 hour
+      gcTime: GC_TIME,
+      staleTime: STALE_TIME,
       structuralSharing: false,
+      // Using a query based persister rather than persisting
+      // the whole state on each query change for performance reasons
+      persister: experimental_createPersister({
+        storage: reactQuerySyncStorage,
+        maxAge: GC_TIME,
+      }),
     },
   },
 });
