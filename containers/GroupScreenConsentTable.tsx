0aa61558905922b09b420ec00d6adbc61d5d599f
Louis Rouffineau <dev@louis.io>
2024-09-17 22:02:31 +0200

    Call the `groupRemoveRestoreHandler` helper from group profile screen

--- containers/GroupScreenConsentTable.tsx
+++ containers/GroupScreenConsentTable.tsx
@@ -1,6 +1,7 @@
 import { useGroupConsent } from "@hooks/useGroupConsent";
 import { translate } from "@i18n";
 import { dangerColor, primaryColor } from "@styles/colors";
+import { groupRemoveRestoreHandler } from "@utils/groupUtils/groupActionHandlers";
 import { FC, useMemo } from "react";
 import { useColorScheme } from "react-native";
 
@@ -10,10 +11,12 @@ import TableView, {
 
 interface GroupScreenConsentTableProps {
   topic: string;
+  groupName: string;
 }
 
 export const GroupScreenConsentTable: FC<GroupScreenConsentTableProps> = ({
   topic,
+  groupName,
 }) => {
   const colorScheme = useColorScheme();
   const { consent, allowGroup, blockGroup } = useGroupConsent(topic);
@@ -26,9 +29,15 @@ export const GroupScreenConsentTable: FC<GroupScreenConsentTableProps> = ({
         title: translate("restore_group"),
         titleColor: primaryColor(colorScheme),
         action: () => {
-          allowGroup({
-            includeAddedBy: false,
-            includeCreator: false,
+          const handleAction = groupRemoveRestoreHandler(
+            "denied",
+            colorScheme,
+            groupName,
+            allowGroup,
+            blockGroup
+          );
+          handleAction(() => {
+            // User canceled, no op
           });
         },
       });
@@ -39,16 +48,22 @@ export const GroupScreenConsentTable: FC<GroupScreenConsentTableProps> = ({
         title: translate("remove_group"),
         titleColor: dangerColor(colorScheme),
         action: () => {
-          blockGroup({
-            includeAddedBy: false,
-            includeCreator: false,
+          const handleAction = groupRemoveRestoreHandler(
+            "allowed",
+            colorScheme,
+            groupName,
+            allowGroup,
+            blockGroup
+          );
+          handleAction(() => {
+            // User canceled, no op
           });
         },
       });
     }
 
     return items;
-  }, [consent, allowGroup, blockGroup, colorScheme]);
+  }, [consent, allowGroup, blockGroup, colorScheme, groupName]);
 
   return (
     <TableView
