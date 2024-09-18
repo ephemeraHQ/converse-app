a86ca1d8469504385ef84639d6bd8f2fdb666242
Noé Malzieu <noe.malzieu@gmail.com>
2024-09-18 11:22:20 +0200

    Revert "Revert "WIP - Rainbow MWP support""
    This reverts commit b525fbf5d32d3e760af5c0c932484a9c48f17754.

--- components/Onboarding/WalletSelector.tsx
+++ components/Onboarding/WalletSelector.tsx
@@ -1,3 +1,4 @@
+import { configure as configureCoinbase } from "@coinbase/wallet-mobile-sdk";
 import { translate } from "@i18n";
 import { textSecondaryColor } from "@styles/colors";
 import logger from "@utils/logger";
@@ -151,6 +152,14 @@ export default function WalletSelector() {
                 try {
                   if (w.name === "Coinbase Wallet") {
                     thirdwebConnect(async () => {
+                      // Default Coinbase configuration
+                      configureCoinbase({
+                        callbackURL: new URL(
+                          `https://${config.websiteDomain}/coinbase`
+                        ),
+                        hostURL: new URL("https://wallet.coinbase.com/wsegue"),
+                        hostPackageName: "org.toshi",
+                      });
                       // instantiate wallet
                       const coinbaseWallet = createWallet(
                         "com.coinbase.wallet",
@@ -164,6 +173,32 @@ export default function WalletSelector() {
                       await coinbaseWallet.connect({ client: thirdwebClient });
                       setActiveWallet(coinbaseWallet);
                       return coinbaseWallet;
+                    });
+                  } else if (w.name === "Rainbow") {
+                    thirdwebConnect(async () => {
+                      // Rainbow now uses Mobile Wallet Protocol
+                      // naming in thirdweb still includes Coinbase
+                      // but it now also works for Rainbow!
+                      configureCoinbase({
+                        callbackURL: new URL(
+                          `https://${config.websiteDomain}/coinbase`
+                        ),
+                        hostURL: new URL("rainbow://wsegue"),
+                        hostPackageName: "me.rainbow",
+                      });
+                      // instantiate wallet
+                      const rainbowWallet = createWallet(
+                        "com.coinbase.wallet",
+                        {
+                          appMetadata: config.walletConnectConfig.appMetadata,
+                          mobileConfig: {
+                            callbackURL: `https://${config.websiteDomain}/coinbase`,
+                          },
+                        }
+                      );
+                      await rainbowWallet.connect({ client: thirdwebClient });
+                      setActiveWallet(rainbowWallet);
+                      return rainbowWallet;
                     });
                   } else if (w.name === "EthOS Wallet") {
                     const signer = getEthOSSigner();
