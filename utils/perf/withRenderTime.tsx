5312f325904b7f1d506e858d323915c932b4f520
Alex Risch <alex@xmtp.com>
2024-09-24 10:50:16 -0500

    feat: Why Did you Render
    Adds why did you render
    
    Adds HOC for performance checks

---
+++ utils/perf/withRenderTime.tsx
@@ -0,0 +1,17 @@
+import React, { ComponentType, useEffect } from "react";
+
+export const withRenderTime = <P extends object>(
+  WrappedComponent: ComponentType<P>,
+  name: string
+): ComponentType<P> => {
+  return (props: P) => {
+    const start = performance.now();
+
+    useEffect(() => {
+      const end = performance.now();
+      console.log(`${name} render time: ${end - start} ms`);
+    });
+
+    return <WrappedComponent {...props} />;
+  };
+};
