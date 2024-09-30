b4debd52c316094496644673d555ca5946518b62
Noé Malzieu <noe.malzieu@gmail.com>
2024-09-30 15:12:20 +0200

    Upgrade SDK to fix group streaming issues

--- ios/ConverseNotificationExtension/Spam.swift
+++ ios/ConverseNotificationExtension/Spam.swift
@@ -92,28 +92,28 @@ func computeSpamScoreGroupWelcome(client: XMTP.Client, group: XMTP.Group, apiURI
   do {
     let consentList = try await client.contacts.refreshConsentList()
     // Probably an unlikely case until consent proofs for groups exist
-    let groupAllowed = await client.contacts.isGroupAllowed(groupId: group.id)
+    let groupAllowed = try await client.contacts.isGroupAllowed(groupId: group.id)
     if groupAllowed {
       return -1
     }
     let inviterInboxId = try group.addedByInboxId()
-    let inviterAllowed = await client.contacts.isInboxAllowed(inboxId: inviterInboxId)
+    let inviterAllowed = try await client.contacts.isInboxAllowed(inboxId: inviterInboxId)
     if inviterAllowed {
       return -1
     }
-    let inviterDenied = await client.contacts.isInboxDenied(inboxId: inviterInboxId)
+    let inviterDenied = try await client.contacts.isInboxDenied(inboxId: inviterInboxId)
     if inviterDenied {
       return 1
     }
-    let members = try group.members
+    let members = try await group.members
     if let inviterAddresses = members.first(where: {$0.inboxId == inviterInboxId})?.addresses {
       for address in inviterAddresses {
-        if await client.contacts.isDenied(EthereumAddress(address).toChecksumAddress()) {
+        if try await client.contacts.isDenied(EthereumAddress(address).toChecksumAddress()) {
           return 1
         }
       }
       for address in inviterAddresses {
-        if await client.contacts.isAllowed(EthereumAddress(address).toChecksumAddress()) {
+        if try await client.contacts.isAllowed(EthereumAddress(address).toChecksumAddress()) {
           return -1
         }
       }
@@ -135,39 +135,39 @@ func computeSpamScoreGroupMessage(client: XMTP.Client, group: XMTP.Group, decode
   do {
     
     try await client.contacts.refreshConsentList()
-    let groupDenied = await client.contacts.isGroupDenied(groupId: group.id)
+    let groupDenied = try await client.contacts.isGroupDenied(groupId: group.id)
     if groupDenied {
       // Network consent will override other checks
       return 1
     }
     let senderInboxId = decodedMessage.senderAddress
-    let senderDenied = await client.contacts.isInboxDenied(inboxId: senderInboxId)
+    let senderDenied = try await client.contacts.isInboxDenied(inboxId: senderInboxId)
     if senderDenied {
       // Network consent will override other checks
       return 1
     }
     
-    let senderAllowed = await client.contacts.isInboxAllowed(inboxId: senderInboxId)
+    let senderAllowed = try await client.contacts.isInboxAllowed(inboxId: senderInboxId)
     if senderAllowed {
       // Network consent will override other checks
       return -1
     }
     
     
-    let groupAllowed = await client.contacts.isGroupAllowed(groupId: group.id)
+    let groupAllowed = try await client.contacts.isGroupAllowed(groupId: group.id)
     if groupAllowed {
       // Network consent will override other checks
       return -1
     }
     
-    if let senderAddresses = try group.members.first(where: {$0.inboxId == senderInboxId})?.addresses {
+    if let senderAddresses = try await group.members.first(where: {$0.inboxId == senderInboxId})?.addresses {
       for address in senderAddresses {
-        if await client.contacts.isDenied(EthereumAddress(address).toChecksumAddress()) {
+        if try await client.contacts.isDenied(EthereumAddress(address).toChecksumAddress()) {
           return 1
         }
       }
       for address in senderAddresses {
-        if await client.contacts.isAllowed(EthereumAddress(address).toChecksumAddress()) {
+        if try await client.contacts.isAllowed(EthereumAddress(address).toChecksumAddress()) {
           return -1
         }
       }
