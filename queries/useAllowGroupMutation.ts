275c201d86062c1464fbc08674047c13f3019632
Michael Lustig <lustig@knophy.com>
2024-10-01 13:28:16 -0400

    docs

--- queries/useAllowGroupMutation.ts
+++ queries/useAllowGroupMutation.ts
@@ -41,3 +41,34 @@ export const useAllowGroupMutation = (account: string, topic: string) => {
     },
   });
 };
+
+// export const useAllowGroupMutation = (account: string, topic: string) => {
+//   const { groupId } = useGroupId(topic);
+//   return useMutation({
+//     mutationKey: allowGroupMutationKey(account, topic),
+//     mutationFn: async () => {
+//       if (!groupId || !account) {
+//         return;
+//       }
+//       await consentToGroupsOnProtocol(account, [groupId], "allow");
+//       return "allowed";
+//     },
+//     onMutate: async () => {
+//       await cancelGroupConsentQuery(account, topic);
+//       const previousConsent = getGroupConsentQueryData(account, topic);
+//       setGroupConsentQueryData(account, topic, "allowed");
+//       return { previousConsent };
+//     },
+//     onError: (error, _variables, context) => {
+//       logger.warn("onError useAllowGroupMutation");
+//       sentryTrackError(error);
+//       if (context?.previousConsent === undefined) {
+//         return;
+//       }
+//       setGroupConsentQueryData(account, topic, context.previousConsent);
+//     },
+//     onSuccess: () => {
+//       logger.debug("onSuccess useAllowGroupMutation");
+//     },
+//   });
+// };
