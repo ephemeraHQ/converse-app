819f2d899ddeca4e471504e9bc316102a180fc09
Michael Lustig <lustig@knophy.com>
2024-10-04 16:34:13 -0400

    Have something almost workable in state machine land

--- features/GroupInvites/joinGroup.machine.ts
+++ features/GroupInvites/joinGroup.machine.ts
@@ -1,14 +1,35 @@
 import { currentAccount } from "@data/store/accountsStore";
-import { getGroupInvite, GroupInvite } from "@utils/api";
-import { fromPromise, setup } from "xstate";
+import {
+  createGroupJoinRequest,
+  getGroupInvite,
+  getGroupJoinRequest,
+  GroupInvite,
+} from "@utils/api";
+import logger from "@utils/logger";
+import { assign, fromPromise, log, setup } from "xstate";
+
+import {
+  getInviteJoinRequestId,
+  saveInviteJoinRequestId,
+} from "./groupInvites.utils";
+
+const GROUP_JOIN_REQUEST_POLL_INTERVAL_MS = 500;
+const GROUP_JOIN_REQUEST_POLL_MAX_ATTEMPTS = 10;
+
+type JoinGroupActorCallbackEvents =
+  | { type: "group-join-request.accepted"; groupId: string }
+  | { type: "group-join-request.already-joined"; groupId: string }
+  | { type: "group-join-request.rejected" }
+  | {
+      type: "group-join-request.error" /* not sure why we don't get an error from the api when we receive this status... error: string*/;
+    }
+  | { type: "group-join-request.timed-out" };
 
 type JoinGroupMachineEvents =
   // user interactions
-  { type: "user.didTapJoinGroup" };
-// | { type: 'user.didTapPositiveFeedback' }
-// | { type: 'user.didTapNegativeFeedback' }
-// | { type: 'user.didTapSubmitFeedback' }
-// | { type: 'user.didTapDoneRating' }
+  | { type: "user.didTapJoinGroup" }
+  // Events received from invoked actors
+  | JoinGroupActorCallbackEvents;
 
 type JoinGroupMachineContext = {
   // Context
@@ -17,75 +38,113 @@ type JoinGroupMachineContext = {
 
   // From Input
   groupInviteId: string;
-  // hasSeenRatingPrompt: boolean;
-  // userInitialLoginDate: string;
-  // isInstallingRft: boolean;
-  // isPairing: boolean;
-  // areHearingAidsConnected: boolean;
-  // externalReviewTrayHasBeenShown: boolean;
-  // isUserRetail: boolean;
-  // userRetailReviewUrl?: string;
-  // groupInvitesThatWereDeterminingRightNow: 'externalReview' | 'internalRating';
+
+  error?: string;
 };
 
 type JoinGroupMachineInput = {
   groupInviteId: string;
 };
 
+type JoinGroupMachineTags = "loading";
+
 export const joinGroupMachineLogic = setup({
   types: {
     events: {} as JoinGroupMachineEvents,
     context: {} as JoinGroupMachineContext,
     input: {} as JoinGroupMachineInput,
+    tags: {} as JoinGroupMachineTags,
   },
 
   actors: {
     fetchGroupInvite: fromPromise<
       GroupInvite,
       { account: string; groupInviteId: string }
-    >(async ({ input /*self, emit, system, signal*/ }) => {
+    >(async ({ input }) => {
       const { groupInviteId } = input;
       // TODO: generic result type
       const groupInvite: GroupInvite = await getGroupInvite(groupInviteId);
       return groupInvite;
     }),
+
+    attemptToJoinGroup: fromPromise<
+      JoinGroupActorCallbackEvents,
+      { account: string; groupInviteId: string }
+    >(async ({ input }) => {
+      const { account, groupInviteId } = input;
+      /*
+       * TODO: replace with Current.joinGroupClient.waitForGroupJoinAcceptance
+       * once it's implemented.
+       *
+       * Use polling and sdk streaming and take first event.
+       *
+       * If the max count is reached, then we'll just, we
+       * consider that a timeout and the inviter is likely
+       * offline or has uninstalled the app.
+       */
+
+      const sleep = (ms: number) =>
+        new Promise((resolve) => setTimeout(resolve, ms));
+
+      let joinRequestId = getInviteJoinRequestId(account, groupInviteId);
+      if (!joinRequestId) {
+        logger.debug(
+          `[GroupInvite] Sending the group join request to Converse backend`
+        );
+        const joinRequest = await createGroupJoinRequest(
+          account,
+          groupInviteId
+        );
+        joinRequestId = joinRequest.id;
+        saveInviteJoinRequestId(account, groupInviteId, joinRequestId);
+      }
+
+      let attemptsToRetryJoinGroup = 0;
+      while (attemptsToRetryJoinGroup < GROUP_JOIN_REQUEST_POLL_MAX_ATTEMPTS) {
+        const joinRequestData = await getGroupJoinRequest(joinRequestId);
+
+        if (joinRequestData.status !== "PENDING") {
+          switch (joinRequestData.status) {
+            case "ACCEPTED":
+              return {
+                type: "group-join-request.accepted",
+                groupId: joinRequestData.groupId as string,
+              };
+            case "REJECTED":
+              return {
+                type: "group-join-request.rejected",
+              };
+            case "ERROR":
+              return {
+                type: "group-join-request.error",
+              };
+          }
+        }
+
+        attemptsToRetryJoinGroup += 1;
+        await sleep(GROUP_JOIN_REQUEST_POLL_INTERVAL_MS);
+      } /* While loop that was polling terminated after some amount of tries */
+
+      return {
+        type: "group-join-request.timed-out",
+      };
+    }),
   },
 
-  guards: {
-    //
-    // isFeedbackEnabled: ({ context }) => {
-    //   const { joinGroupThatWereDeterminingRightNow } = context;
-    //   const flagPropertyName =
-    //     joinGroupThatWereDeterminingRightNow === 'externalReview'
-    //       ? 'externalReviewEnabled'
-    //       : 'internalAppRatingEnabled';
-    //   const variant = Controlled.featureFlagClient.getVariant(FeatureFlags.MOBILE_EXTERNAL_REVIEW_SETTINGS);
-    //   const jsonStr = variant.payload?.value;
-    //   // @ts-expect-error TODO better feature flag  client
-    //   const payload = JSON.parse(jsonStr) as ReviewAndRatingFeatureFlagPayload;
-    //   const flagPropertyValue = payload[flagPropertyName];
-    //   console.log({ flagPropertyValue });
-    //   return flagPropertyValue;
-    // },
-    //
+  actions: {
+    navigateToGroupScreen: log(
+      ({ event }) =>
+        `-> navigateToGroupScreen ${JSON.stringify({
+          question: "Does the event have a groupId?",
+          event,
+        })}`
+    ),
   },
+
+  guards: {},
 }).createMachine({
   id: "joinGroupMachine",
   context: ({ input }) => {
-    const account = currentAccount();
-    const { groupInviteId } = input;
-    console.log({ account, groupInviteId });
-
-    return {
-      account,
-      groupInviteId,
-    };
-  },
-  context: ({ input }) => {
-    // const reviewsAndRatings = entityTracker.getLastEntityValue('reviewsAndRatings');
-    // const flows = entityTracker.getLastEntityValue('flows');
-    // const hearingAids = entityTracker.getLastEntityValue('hearingAids');
-    // const user = entityTracker.getLastEntityValue('realJabraUser');
     const account = currentAccount();
     const { groupInviteId } = input;
     console.log({ account, groupInviteId });
@@ -100,6 +159,12 @@ export const joinGroupMachineLogic = setup({
 
   states: {
     "Loading Group Invite Metadata": {
+      description: `
+Fetches the group invite metadata from the server.
+This metadata contains information that a potential
+joiner will see when they land on the deep link page.
+`,
+      tags: ["loading"],
       invoke: {
         src: "fetchGroupInvite",
         input: ({ context }) => {
@@ -108,254 +173,131 @@ export const joinGroupMachineLogic = setup({
             account: context.account,
           };
         },
-      },
-      onDone: {
-        target: "Checking Invite Status",
-        actions: assign({
-          groupInviteMetadata: (_, event) => event.output,
-        }),
-      },
-      onError: {
-        target: "Error",
-        actions: assign({
-          error: (_, event) => event.data,
-        }),
+        onDone: {
+          description: `
+We should be able to check the invite status
+without creating a groupJoinRequest, but in GroupInvites.screen,
+that isn't how things are done, so I'm going to follow what's currently there.
+
+This requires that the user click the button before we check if they've already
+joined, so I think I'm missing some context.
+`,
+          // target: "Checking Invite Status",
+          target: "Waiting For User Action",
+          actions: assign({
+            groupInviteMetadata: ({ event }) => event.output,
+          }),
+        },
+        onError: {
+          target: "Error Loading Group Invite",
+          actions: assign({
+            error: ({ event }) => JSON.stringify(event.error),
+          }),
+        },
       },
     },
-    "Checking Invite Status": {
-      always: [
-        {
-          guard: "isInviteValid",
-          target: "Waiting For User To Join",
-        },
-        {
-          target: "Invalid Invite",
-        },
-      ],
-    },
-    "Waiting For User To Join": {
+
+    "Waiting For User Action": {
+      description: `
+In this state, the UI will be displaying a button to the user 
+to allow them to begin the group join process.
+
+Some potential improvements to this flow would be to have a state
+prior where we check the status of the group join request, but 
+that isn't how things are done in the current version of the 
+screen so I'm going to follow what's currently there for now.
+`,
       on: {
         "user.didTapJoinGroup": {
-          target: "Sending Join Request",
+          target: "Attempting to Join Group",
         },
       },
     },
-    "Sending Join Request": {
+
+    "Attempting to Join Group": {
+      description: `
+Attempts to join the group. 
+
+Due to the encrypted nature of our protocol, we send a request to the creator 
+of the group invite via Push Notifications that, when received, will 
+automatically accept the join request. 
+
+However, if there is any latency, or if the user that created
+the invite is offline or has uninstalled the application,
+then the group invite will never be accepted.
+
+This is a known limitation of our current implementation,
+and we are exploring ideas such as allowing more admins
+to accept the invite.
+      `,
       invoke: {
-        src: "sendJoinRequest",
+        src: "attemptToJoinGroup",
         input: ({ context }) => {
           return {
-            account: context.account,
             groupInviteId: context.groupInviteId,
+            account: context.account,
           };
         },
-      },
-      onDone: {
-        target: "Polling Join Request Status",
-        actions: assign({
-          joinRequestId: (_, event) => event.output.id,
-        }),
-      },
-      onError: {
-        target: "Error",
-        actions: assign({
-          error: (_, event) => event.data,
-        }),
-      },
-    },
-    "Polling Join Request Status": {
-      invoke: {
-        src: "pollJoinRequestStatus",
-        input: ({ context }) => {
-          return {
-            joinRequestId: context.joinRequestId,
-          };
-        },
-      },
-      onDone: [
-        {
-          guard: "isJoinAccepted",
-          target: "Join Successful",
-        },
-        {
-          guard: "isJoinRejected",
-          target: "Join Rejected",
-        },
-        {
-          target: "Error",
-        },
-      ],
-      onError: {
-        target: "Error",
-        actions: assign({
-          error: (_, event) => event.data,
-        }),
+        onDone: [
+          {
+            guard: ({ event }) =>
+              event.output.type === "group-join-request.accepted",
+            target: "User Joined Group",
+          },
+          {
+            guard: ({ event }) =>
+              event.output.type === "group-join-request.rejected",
+            target: "Request to Join Group Rejected",
+          },
+          {
+            guard: ({ event }) =>
+              event.output.type === "group-join-request.error",
+            target: "Error Joining Group",
+          },
+          {
+            guard: ({ event }) =>
+              event.output.type === "group-join-request.timed-out",
+            target: "Attempting to Join Group Timed Out",
+          },
+        ],
       },
     },
-    "Join Successful": {
+
+    "User Joined Group": {
+      type: "final",
+      actions: ["navigateToGroupScreen"],
+    },
+
+    "Request to Join Group Rejected": {
       type: "final",
     },
-    "Join Rejected": {
+
+    "Attempting to Join Group Timed Out": {
+      description: `
+The invitor client has not yet automatically accepted the
+group join request. This is a known limitation of our current
+implementation, and we are exploring ideas such as allowing
+more admins to accept the invite.
+
+This doesn't mean the user cannot join, it just means that
+the client that was invited needs to wait for the inviter
+to accept the request.
+
+The next time we are able to contact the inviter, we will
+automatically accept the request and the newly invited
+user will be able to join the group.
+`,
       type: "final",
     },
-    "Invalid Invite": {
-      type: "final",
-    },
-    "Error": {
-      type: "final",
-    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    // ERROR STATES
+    ///////////////////////////////////////////////////////////////////////////
+
+    "Error Loading Group Invite": {},
   },
-    //
-    // 'Checking If Feedback Enabled via Feature Flag': {
-    //   always: [
-    //     {
-    //       guard: 'isFeedbackEnabled',
-    //       target: 'Checking If The Feedback Is Due To Show',
-    //     },
-    //     {
-    //       actions: () => {
-    //         return assign({
-    //           joinGroupThatWereDeterminingRightNow: 'internalRating',
-    //         });
-    //       },
-    //       target: 'Checking If Feedback Enabled via Feature Flag',
-    //       reenter: true,
-    //     },
-    //   ],
-    // },
-    //
-    // 'Checking If The Feedback Is Due To Show': {
-    //   always: [
-    //     {
-    //       guard: 'isFeedbackDue',
-    //       target: 'Checking If Hearing Aids Are Connected',
-    //     },
-    //     {
-    //       actions: () => {
-    //         return assign({
-    //           joinGroupThatWereDeterminingRightNow: 'internalRating',
-    //         });
-    //       },
-    //       target: 'Checking If Feedback Enabled via Feature Flag',
-    //       reenter: true,
-    //     },
-    //   ],
-    // },
-    //
-    // 'Checking If Hearing Aids Are Connected': {
-    //   always: [
-    //     { guard: 'areHearingAidsConnected', target: 'Checking If The Feedback Tray Has Been Shown' },
-    //     {
-    //       actions: () => {
-    //         return assign({
-    //           groupInvitesThatWereDeterminingRightNow: 'internalRating',
-    //         });
-    //       },
-    //       target: 'Checking If Feedback Enabled via Feature Flag',
-    //       reenter: true,
-    //     },
-    //   ],
-    // },
-    //
-    // 'Checking If The Feedback Tray Has Been Shown': {
-    //   always: [
-    //     { guard: 'feedbackTrayHasNotBeenShown', target: 'Checking If The User Is Retail' },
-    //     {
-    //       actions: () => {
-    //         return assign({
-    //           groupInvitesThatWereDeterminingRightNow: 'internalRating',
-    //         });
-    //       },
-    //       target: 'Checking If Feedback Enabled via Feature Flag',
-    //       reenter: true,
-    //     },
-    //   ],
-    // },
-    //
-    // 'Checking If The User Is Retail': {
-    //   always: [
-    //     { guard: 'isUserRetail', target: 'Checking If App Is In An Important Flow' },
-    //     {
-    //       actions: () => {
-    //         return assign({
-    //           groupInvitesThatWereDeterminingRightNow: 'internalRating',
-    //         });
-    //       },
-    //       target: 'Checking If Feedback Enabled via Feature Flag',
-    //       reenter: true,
-    //     },
-    //   ],
-    // },
-    //
-    // 'Checking If App Is In An Important Flow': {
-    //   always: [
-    //     { guard: 'appIsNotInAnImportantFlow', target: 'Feedback Timer Counting Down' },
-    //     { target: 'Waiting For App Flow Completion' },
-    //   ],
-    // },
-    //
-    // 'Feedback Timer Counting Down': {
-    //   after: {
-    //     30_000: { target: 'Transient Determine Which Prompt to Show' },
-    //   },
-    //   on: {
-    //     // todo we could do an always and check if the event represents
-    //     // a flow starting...
-    //     'flow.started': { target: 'Waiting For App Flow Completion' },
-    //     'emitted.storage.updated.flows': {
-    //       actions: ({ event }) => {
-    //         console.log({ event });
-    //         const { pairing, installingRft } = event.data;
-    //         const isInAnImportantFlow = pairing || installingRft;
-    //         if (isInAnImportantFlow) {
-    //           raise({ type: 'flow.started' });
-    //         }
-    //       },
-    //     },
-    //   },
-    // },
-    //
-    // 'Transient Determine Which Prompt to Show': {
-    //   always: [
-    //     { guard: 'isExternalReview', target: 'Showing External Review Prompt' },
-    //     { guard: 'isInternalRating', target: 'Showing Internal Rating Prompt' },
-    //     { target: 'Not Going To Show This Session' },
-    //   ],
-    // },
-    //
-    // 'Waiting For App Flow Completion': {
-    //   on: {
-    //     'flow.completed': { target: 'Feedback Timer Counting Down' },
-    //
-    //     'emitted.storage.updated.flows': {
-    //       actions: ({ event }) => {
-    //         const { pairing, installingRft } = event.data;
-    //         const isInAnImportantFlow = pairing || installingRft;
-    //         if (!isInAnImportantFlow) {
-    //           raise({ type: 'flow.completed' });
-    //         }
-    //       },
-    //     },
-    //   },
-    // },
-    //
-    // 'Showing External Review Prompt': {
-    //   on: {
-    //     // 'user.didTapWriteAReview': { target: 'Checking If Internal Rating Enabled via Feature Flag' },
-    //     // 'user.didTapDismiss': { target: 'Checking If Internal Rating Enabled via Feature Flag' },
-    //   },
-    // },
-    //
-    // 'Showing Internal Rating Prompt': {
-    //   on: {
-    //     'user.didTapPositiveFeedback': { target: 'Not Going To Show This Session' },
-    //     'user.didTapNegativeFeedback': { target: 'Not Going To Show This Session' },
-    //     'user.didTapDismiss': { target: 'Not Going To Show This Session' },
-    //   },
-    // },
-    //
-    // 'Not Going To Show This Session': {
-    //   type: 'final',
-    // },
+
+  "Error Joining Group": {
+    type: "final",
   },
 });
