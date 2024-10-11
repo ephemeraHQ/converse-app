9d6be40bf54e006ba3be8288d72d21c3f43b3fee
Thierry Skoda <6875849+thierryskoda@users.noreply.github.com>
2024-10-11 12:58:40 -0400

    feat: add theme + start of design system + haptics (#963)
    * add theme + start of design system + haptics
    
    * add theme provider
    
    * add babel config
    
    * remove try catch haptics and remove $ in styles
    
    * fix tsc

---
+++ design-system/Text/Text.presets.ts
@@ -0,0 +1,30 @@
+import { TextStyle } from "react-native";
+
+import {
+  textBaseStyle,
+  textFontWeightStyles,
+  textSizeStyles,
+} from "./Text.styles";
+import { ThemedStyleArray } from "../../theme/useAppTheme";
+
+export type IPresets =
+  | "default"
+  | "bold"
+  | "heading"
+  | "subheading"
+  | "formLabel"
+  | "formHelper";
+
+export const presets: Record<IPresets, ThemedStyleArray<TextStyle>> = {
+  default: [textBaseStyle],
+
+  bold: [textBaseStyle, textFontWeightStyles.bold],
+
+  heading: [textBaseStyle, textSizeStyles.xxl, textFontWeightStyles.semiBold],
+
+  subheading: [textBaseStyle, textSizeStyles.sm, textFontWeightStyles.light],
+
+  formLabel: [textBaseStyle, textFontWeightStyles.medium],
+
+  formHelper: [textBaseStyle, textSizeStyles.sm, textFontWeightStyles.normal],
+};
