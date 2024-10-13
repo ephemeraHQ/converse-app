eedb2eafb9c6a8acf42fb138dc15af8072c6de3e
Alex Risch <alex.j.risch@gmail.com>
2024-10-13 18:34:40 -0400

    feat: Notify When Group Join Fails (#949)
    * feat: Notifiy When Group Join Fails
    
    When silent handling of group join requests failed then notify the user
    
    * feat: Notify on Group Add Fail
    
    Fixed Error handling in Swift
    Fixed weird navigation when navigating to group
    Removed local invite check
    Added push handling for data/mutable notification
    Added check when attempting to join
    
    * update strings
    
    ---------
    
    Co-authored-by: Alex Risch <alex@xmtp.com>

--- ios/ConverseNotificationExtension/NotificationService.swift
+++ ios/ConverseNotificationExtension/NotificationService.swift
@@ -144,14 +144,18 @@ func handleConverseNotification(contentHandler: ((UNNotificationContent) -> Void
         contentHandler(UNNotificationContent())
         return
       }
-      await handleGroupJoinRequestNotification(
+      let shouldShowNotification = await handleGroupJoinRequestNotification(
         groupInviteId: groupInviteId,
         joinRequestId: joinRequestId,
         address: address,
         account: account,
         content: content
       )
-      contentHandler(UNNotificationContent())
+      if (shouldShowNotification) {
+        contentHandler(content)
+      } else {
+        contentHandler(UNNotificationContent())
+      }
       break
     default:
       contentHandler(UNNotificationContent())
@@ -160,33 +164,36 @@ func handleConverseNotification(contentHandler: ((UNNotificationContent) -> Void
     
   }
   
-  func handleGroupJoinRequestNotification(groupInviteId: String, joinRequestId: String, address: String, account: String, content: UNMutableNotificationContent) async {
+  func handleGroupJoinRequestNotification(groupInviteId: String, joinRequestId: String, address: String, account: String, content: UNMutableNotificationContent) async -> Bool {
     do {
-      
       let mmkv = getMmkv()
-      if let xmtpClient = await getXmtpClient(account: account){
+      if let xmtpClient = await getXmtpClient(account: account) {
         guard let groupId = mmkv?.string(forKey: "group-invites-link-\(groupInviteId)") else {
           // Don't handle this as it's stored on a different device
-          return
+          return false // Do not show notification
         }
         var apiURI = mmkv?.string(forKey: "api-uri")
         if (apiURI == nil) {
           let sharedDefaults = try! SharedDefaults()
           apiURI = sharedDefaults.string(forKey: "api-uri")?.replacingOccurrences(of: "\"", with: "")
         }
-        
-        if let group = await getGroup(xmtpClient: xmtpClient, groupId: groupId), let apiURI = apiURI {
-          try await group.addMembers(addresses: [address])
-          await putGroupInviteRequest(apiURI: apiURI, account: account, xmtpClient: xmtpClient, status: "ACCEPTED", joinRequestId: joinRequestId)
+        do {
+          if let group = await getGroup(xmtpClient: xmtpClient, groupId: groupId), let apiURI = apiURI {
+            try await group.addMembers(addresses: [address])
+            try await putGroupInviteRequest(apiURI: apiURI, account: account, xmtpClient: xmtpClient, status: "ACCEPTED", joinRequestId: joinRequestId)
+            return false // Do not show notification when successfully added
+          }
+        } catch {
+          sentryTrackError(error: error, extras: ["message": "Could not add member to group"])
+          return true // Show notification if any of these errors occurs
         }
       } else {
         sentryTrackMessage(message: "No client found for account", extras: ["account": account])
       }
     } catch {
       sentryTrackError(error: error, extras: ["message": "Could not get or sync group"])
-      
     }
-    
+    return false // Do not show notification if any error occurs, could not have the client or mmkv
   }
 }
 
