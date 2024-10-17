655dc084fe2c4d57d3d544dc25292ce21eb33c7c
Thierry Skoda <6875849+thierryskoda@users.noreply.github.com>
2024-10-17 09:58:24 -0400

    recs for cleaner (#1012)

--- design-system/Text/Text.utils.ts
+++ design-system/Text/Text.utils.ts
@@ -1,4 +1,4 @@
-import { useAppTheme } from "@theme/useAppTheme";
+import { IThemed } from "@theme/useAppTheme";
 import { StyleProp, TextStyle } from "react-native";
 
 import { IPresets, textPresets } from "./Text.presets";
@@ -6,7 +6,7 @@ import { ITextStyleProps } from "./Text.props";
 import { textFontWeightStyles, textSizeStyles } from "./Text.styles";
 
 export const getTextStyle = (
-  themed: ReturnType<typeof useAppTheme>["themed"],
+  themed: IThemed,
   { weight, size, style: styleProp, ...props }: ITextStyleProps
 ): StyleProp<TextStyle> => {
   const preset: IPresets = props.preset ?? "body";
