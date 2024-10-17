c82b0aeac58503dfdd73bfeb2a1cf3746d9beeb9
Thierry <skodathierry@gmail.com>
2024-10-17 10:36:50 -0400

    fix hstack file name

--- design-system/Center.tsx
+++ design-system/Center.tsx
@@ -2,7 +2,7 @@ import { ForwardedRef, forwardRef } from "react";
 import { View } from "react-native";
 import Animated, { AnimatedProps } from "react-native-reanimated";
 
-import { HStack } from "./HStack-tmp";
+import { HStack } from "./HStack";
 
 export type ICenterProps = IHStackProps;
 
