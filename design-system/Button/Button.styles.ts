28159be9f2a3c75a70f9038acc2168fe947605a8
Thierry Skoda <6875849+thierryskoda@users.noreply.github.com>
2024-10-18 19:46:45 -0400

    feat: more design system stuff (#1006)
    * wip
    
    * more color fix
    
    * fix hstack file name
    
    * fix hstack file name
    
    * fix hstack file name
    
    * fix types + fix button + add more theme stuff
    
    * fix button
    
    * more fixes
    
    * fix snapshots
    
    * wip
    
    * bottom sheet wip
    
    * fix button component and refactor bottom sheet into multiple files
    
    * more bottom sheet fixes
    
    * clean up
    
    * fix

---
+++ design-system/Button/Button.styles.ts
@@ -0,0 +1,138 @@
+import { TextStyle, ViewStyle } from "react-native";
+
+import { Theme, ThemedStyle } from "../../theme/useAppTheme";
+import { textPresets } from "./../Text/Text.presets";
+import { IButtonAction, IButtonSize, IButtonVariant } from "./Button.props";
+
+type IButtonStyleProps = {
+  variant: IButtonVariant;
+  size: IButtonSize;
+  action: IButtonAction;
+  pressed?: boolean;
+  disabled?: boolean;
+};
+
+export const $buttonRightAccessoryStyle: ThemedStyle<ViewStyle> = ({
+  spacing,
+}) => ({
+  marginStart: spacing.xs,
+  zIndex: 1,
+});
+
+export const $buttonLeftAccessoryStyle: ThemedStyle<ViewStyle> = ({
+  spacing,
+}) => ({
+  marginEnd: spacing.xs,
+  zIndex: 1,
+});
+
+export const getButtonViewStyle =
+  ({
+    variant,
+    size,
+    action,
+    pressed = false,
+    disabled = false,
+  }: IButtonStyleProps) =>
+  (theme: Theme): ViewStyle => {
+    const { spacing, colors, borderRadius } = theme;
+
+    const style: ViewStyle = {
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: borderRadius.sm,
+      overflow: "hidden",
+      paddingVertical: size === "md" ? spacing.xxs : spacing.xs,
+      paddingHorizontal: size === "md" ? spacing.xs : spacing.sm,
+    };
+
+    if (action === "primary") {
+      switch (variant) {
+        case "fill":
+          style.backgroundColor = colors.fill.primary;
+          if (pressed) {
+            style.backgroundColor = colors.fill.secondary;
+          }
+          if (disabled) {
+            style.backgroundColor = colors.fill.tertiary;
+          }
+          break;
+
+        case "outline":
+          style.borderWidth = 1;
+          style.borderColor = colors.border.secondary;
+          style.backgroundColor = "transparent";
+          if (pressed) {
+            style.backgroundColor = colors.fill.minimal;
+          }
+          break;
+
+        case "link":
+        case "text":
+          style.backgroundColor = "transparent";
+          // Put back when we're done refactoring all the variant="text" button
+          // if (pressed) {
+          //   style.backgroundColor = colors.fill.minimal;
+          // }
+          // Temporary opacity change for the variant="text" button
+          if (pressed) {
+            style.opacity = 0.8;
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    return style;
+  };
+
+export const getButtonTextStyle =
+  ({ variant, action, pressed = false, disabled = false }: IButtonStyleProps) =>
+  (theme: Theme): TextStyle => {
+    const { colors } = theme;
+
+    const style: TextStyle = {
+      ...textPresets.body,
+      textAlign: "center",
+      flexShrink: 1,
+      flexGrow: 0,
+      zIndex: 2,
+    };
+
+    if (action === "primary") {
+      switch (variant) {
+        case "fill":
+          style.color = colors.text.inverted.primary;
+          break;
+
+        case "outline":
+          style.color = colors.text.primary;
+          if (disabled) {
+            style.color = colors.fill.tertiary;
+          }
+          break;
+
+        case "link":
+        case "text":
+          style.color = colors.text.primary;
+          if (disabled) {
+            style.color = colors.fill.tertiary;
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    return style;
+  };
+
+// Cursor query
+// Ok I'll tell you some styling instruction and please make sure the styles are done correctly. All of those are for action = primary. We don't have other styling yet for the action = danger. Also don,t forget the size (lg or md)
+// if variant is filled. background is is fill.primary and text is text.inverted.primary. If pressed is true, fill is secondary, no opacity change. if disabled fill is tertiary, no opacity change. and the text stays the same.
+// if variant is outline, border is border.secondary, text is text.primary and background transparent. if pressed is true , background is fill.minimal. if disabled, text is fill.tertiary, border is border.secondary, background is transparent.
+// if variant is link, background is transparent, text is primary. if pressed = true, background is now fill.minimal. if disabled, text is fill.tertiary.
