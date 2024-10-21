f97d78cadb59b5d59f54fdff04a1faeacedc5b7d
Noé Malzieu <noe.malzieu@gmail.com>
2024-10-21 09:38:23 +0200

    Keep testing SCW

--- screens/SCWTest.tsx
+++ screens/SCWTest.tsx
@@ -1,8 +1,9 @@
 import { thirdwebClient } from "@utils/thirdweb";
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import React, { useCallback } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { prepareTransaction } from "thirdweb";
-import { sepolia } from "thirdweb/chains";
+import { ethers5Adapter } from "thirdweb/adapters/ethers5";
+import { base } from "thirdweb/chains";
 import {
   useActiveAccount,
   useConnect,
@@ -20,7 +21,8 @@ const MainScreen: React.FC = () => {
   const sendTransaction = useSendTransaction();
   const setActiveWallet = useSetActiveWallet();
   const switchChain = useSwitchActiveWalletChain();
-  const handleButtonPress = () => {
+
+  const handleButtonPress = useCallback(() => {
     thirdwebConnect(async () => {
       // instantiate wallet
       const coinbaseWallet = createWallet("com.coinbase.wallet", {
@@ -42,35 +44,58 @@ const MainScreen: React.FC = () => {
       }
       return coinbaseWallet;
     });
-  };
-  const handleTransaction = async () => {
+  }, [thirdwebConnect, setActiveWallet]);
+
+  const handleTransaction = useCallback(async () => {
+    console.log("clicked!");
     const transaction = prepareTransaction({
       to: "0x2376e9C7C604D1827bA9aCb1293Dc8b4DA2f0DB3",
-      value: BigInt(1),
-      chain: sepolia,
+      value: BigInt(10000000000000),
+      chain: base,
       client: thirdwebClient,
     });
     console.log("transaction", transaction);
     try {
-      await switchChain(sepolia);
+      await switchChain(base);
       console.log("switched chain");
-      // await sendTransaction.mutateAsync(transaction);
-      // console.log("sent transaction");
-    } catch (e) {
+      const tx = await sendTransaction.mutateAsync(transaction);
+      console.log("sent transaction", tx.transactionHash);
+    } catch (e: any) {
+      alert(e.message);
       console.log(e);
     }
-  };
+  }, [switchChain, sendTransaction]);
+
+  const handleSignature = useCallback(async () => {
+    if (!activeAccount) return;
+    await switchChain(base);
+    const signer = await ethers5Adapter.signer.toEthers({
+      client: thirdwebClient,
+      chain: base,
+      account: activeAccount,
+    });
+    const signature = await signer.signMessage("Hello, world");
+    console.log("base", signature);
+  }, [activeAccount, switchChain]);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
-        {!activeAccount && (
+      {!activeAccount && (
+        <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
           <Text style={styles.buttonText}>Connect to wallet</Text>
-        )}
-        {activeAccount && (
-          <Button onPress={handleTransaction} title="TRIGGER TX" />
-        )}
-      </TouchableOpacity>
+        </TouchableOpacity>
+      )}
+      {activeAccount && (
+        <>
+          <Text>Address: {activeAccount.address}</Text>
+          <TouchableOpacity style={styles.button} onPress={handleTransaction}>
+            <Text style={styles.buttonText}>Trigger tx!</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleSignature}>
+            <Text style={styles.buttonText}>Trigger signature</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 };
