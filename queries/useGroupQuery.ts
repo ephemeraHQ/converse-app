267c1c365e2475195d59ce1f6caf9418f745439f
Alex Risch <alex.j.risch@gmail.com>
2024-10-23 13:11:38 -0400

    feat: V3 Split WIP

--- queries/useGroupQuery.ts
+++ queries/useGroupQuery.ts
@@ -1,23 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCleanAddress } from "@utils/evm/address";
 import { getGroupIdFromTopic, isGroupTopic } from "@utils/groupUtils/groupId";
-import {
-  ConverseXmtpClientType,
-  xmtpClientByAccount,
-} from "@utils/xmtpRN/client";
+import { ConverseXmtpClientType } from "@utils/xmtpRN/client";
 import { getXmtpClient } from "@utils/xmtpRN/sync";
 import { Group } from "@xmtp/react-native-sdk";
 
 import { groupQueryKey } from "./QueryKeys";
-import { entifyWithAddress } from "./entify";
 import { queryClient } from "./queryClient";
-import { setGroupMembersQueryData } from "./useGroupMembersQuery";
-import { setGroupNameQueryData } from "./useGroupNameQuery";
-import { setGroupPhotoQueryData } from "./useGroupPhotoQuery";
-import { useGroupsQuery } from "./useGroupsQuery";
 
 export const useGroupQuery = (account: string, topic: string) => {
-  const { data, dataUpdatedAt } = useGroupsQuery(account);
   return useQuery({
     queryKey: groupQueryKey(account, topic),
     queryFn: async () => {
@@ -28,55 +18,38 @@ export const useGroupQuery = (account: string, topic: string) => {
       if (!client) {
         return null;
       }
-      let group = data?.byId[topic];
-      let groupDataUpdatedAt = dataUpdatedAt;
-      if (!group) {
-        group = await client?.conversations.findGroup(
-          getGroupIdFromTopic(topic)
-        );
-        groupDataUpdatedAt = new Date().getTime();
-        if (!group) {
-          return null;
-        }
-      }
-      // We'll pre-cache some queries since we know
-      // how old is our current group instance
-      setGroupNameQueryData(account, topic, group.name, {
-        updatedAt: groupDataUpdatedAt,
-      });
-      setGroupPhotoQueryData(account, topic, group.imageUrlSquare, {
-        updatedAt: groupDataUpdatedAt,
-      });
-      setGroupMembersQueryData(
-        account,
-        topic,
-        entifyWithAddress(
-          group.members,
-          (member) => member.inboxId,
-          // TODO: Multiple addresses support
-          (member) => getCleanAddress(member.addresses[0])
-        ),
-        {
-          updatedAt: groupDataUpdatedAt,
-        }
+      const group = await client.conversations.findGroup(
+        getGroupIdFromTopic(topic)
       );
+
       return group;
     },
-    enabled: !!data && isGroupTopic(topic),
-    select: (data) => {
-      if (!data) {
+    enabled: isGroupTopic(topic),
+  });
+};
+
+export const useGroupConversationScreenQuery = (
+  account: string,
+  topic: string
+) => {
+  return useQuery({
+    queryKey: groupQueryKey(account, topic),
+    queryFn: async () => {
+      if (!topic) {
         return null;
       }
-      if (data instanceof Group) {
-        return data;
-      }
-      const client = xmtpClientByAccount[account];
+      const client = (await getXmtpClient(account)) as ConverseXmtpClientType;
       if (!client) {
         return null;
       }
-      // Recreate the group object with the client
-      return new Group(client!, data, (data as any)?.members);
+      const group = await client.conversations.findGroup(
+        getGroupIdFromTopic(topic)
+      );
+      await group?.sync();
+
+      return group;
     },
+    enabled: isGroupTopic(topic),
   });
 };
 
---
+++ utils/mergeLists.ts
@@ -0,0 +1,23 @@
+export function mergeOrderedLists<L1, L2>(
+  list1: L1[],
+  list2: L2[],
+  list1Order: (a: L1) => number,
+  list2Order: (a: L2) => number
+): (L1 | L2)[] {
+  // Merge 2 sorted lists
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < list1.length && j < list2.length) {
+    if (list1Order(list1[i]) < list2Order(list2[j])) {
+      merged.push(list1[i]);
+      i++;
+    } else {
+      merged.push(list2[j]);
+      j++;
+    }
+  }
+
+  return merged;
+}
