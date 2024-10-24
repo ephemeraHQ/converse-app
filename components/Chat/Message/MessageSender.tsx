113ecc126d225a88af2419fdddae2a134f2a6aad
Alex Risch <alex.j.risch@gmail.com>
2024-10-24 17:31:11 -0400

    Finalized Conversation List Component Started some work on V3 Message Component Cleaned up several Components Cleaned up list merging Better query handling for messages

---
+++ components/Chat/Message/MessageSender.tsx
@@ -0,0 +1,63 @@
+import { useInboxIdStore, useProfilesStore } from "@data/store/accountsStore";
+import { textSecondaryColor } from "@styles/colors";
+import { getPreferredName, getProfile } from "@utils/profile";
+import { useMemo } from "react";
+import { StyleSheet, useColorScheme, View, Text } from "react-native";
+
+type MessageSenderDumbProps = {
+  name: string;
+};
+
+const MessageSenderDumb = ({ name }: MessageSenderDumbProps) => {
+  const styles = useStyles();
+  return (
+    <View style={styles.groupSenderWrapper}>
+      <Text style={styles.groupSender}>{name}</Text>
+    </View>
+  );
+};
+
+type MessageSenderProps = {
+  senderAddress: string;
+};
+
+export const MessageSender = ({ senderAddress }: MessageSenderProps) => {
+  const senderSocials = useProfilesStore(
+    (s) => getProfile(senderAddress, s.profiles)?.socials
+  );
+  const name = getPreferredName(senderSocials, senderAddress);
+  return <MessageSenderDumb name={name} />;
+};
+
+type V3MessageSenderProps = {
+  inboxId: string;
+};
+
+export const V3MessageSender = ({ inboxId }: V3MessageSenderProps) => {
+  const address = useInboxIdStore((s) => s.byInboxId[inboxId]?.[0]);
+  const senderSocials = useProfilesStore(
+    (s) => getProfile(address, s.profiles)?.socials
+  );
+  const name = getPreferredName(senderSocials, address);
+  return <MessageSenderDumb name={name} />;
+};
+
+const useStyles = () => {
+  const colorScheme = useColorScheme();
+  return useMemo(
+    () =>
+      StyleSheet.create({
+        groupSenderWrapper: {
+          flexDirection: "row",
+          flexBasis: "100%",
+        },
+        groupSender: {
+          fontSize: 12,
+          color: textSecondaryColor(colorScheme),
+          marginLeft: 10,
+          marginVertical: 4,
+        },
+      }),
+    [colorScheme]
+  );
+};
--- queries/useGroupMessages.ts
+++ queries/useGroupMessages.ts
@@ -3,10 +3,12 @@ import { useQuery } from "@tanstack/react-query";
 import { groupMessagesQueryKey } from "./QueryKeys";
 import { entify } from "./entify";
 import { useGroupConversationScreenQuery } from "./useGroupQuery";
+import { useRefreshOnFocus } from "./useRefreshOnFocus";
 
 export const useGroupMessages = (account: string, topic: string) => {
   const { data: group } = useGroupConversationScreenQuery(account, topic);
-  return useQuery({
+
+  const queryData = useQuery({
     queryKey: groupMessagesQueryKey(account, topic),
     queryFn: async () => {
       if (!group) {
@@ -20,4 +22,8 @@ export const useGroupMessages = (account: string, topic: string) => {
     },
     enabled: !!group,
   });
+
+  useRefreshOnFocus(queryData.refetch);
+
+  return queryData;
 };
