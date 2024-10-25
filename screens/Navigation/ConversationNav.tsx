5595159cab282d372dd1860929f86941dc7e1d2b
Thierry <skodathierry@gmail.com>
2024-10-25 11:12:21 -0400

    rebase 2.0.8

--- screens/Navigation/ConversationNav.tsx
+++ screens/Navigation/ConversationNav.tsx
@@ -10,14 +10,13 @@ import {
 } from "react-native";
 import { StackAnimationTypes } from "react-native-screens";
 
-import {
-  NativeStack,
-  navigationAnimation,
-  NavigationParamList,
-} from "./Navigation";
-import { useIsSplitScreen } from "./navHelpers";
 import Picto from "../../components/Picto/Picto";
 import Conversation from "../Conversation";
+import {
+  NativeStack,
+  NavigationParamList,
+  navigationAnimation,
+} from "./Navigation";
 
 export type ConversationNavParams = {
   topic?: string;
@@ -39,10 +38,6 @@ export const ConversationScreenConfig = {
 export default function ConversationNav(
   routeParams?: ConversationNavParams | undefined
 ) {
-  // If we're in split screen mode, the topic is not passed via the usual StackNavigation
-  // but via the DrawerNavigation that passes it back to this component via prop
-  // so we override the route when instantiating Conversation
-  const isSplitScreen = useIsSplitScreen();
   const colorScheme = useColorScheme();
   const navigationOptions = useCallback(
     ({ navigation }: { navigation: NavigationProp<NavigationParamList> }) => ({
@@ -72,18 +67,7 @@ export default function ConversationNav(
   return (
     <NativeStack.Screen name="Conversation" options={navigationOptions}>
       {({ route, navigation }) => (
-        <Conversation
-          navigation={navigation}
-          key={
-            isSplitScreen
-              ? `conversation-${JSON.stringify(routeParams || {})}`
-              : "conversation"
-          }
-          route={{
-            ...route,
-            params: isSplitScreen ? routeParams || {} : route.params,
-          }}
-        />
+        <Conversation navigation={navigation} route={route} />
       )}
     </NativeStack.Screen>
   );
