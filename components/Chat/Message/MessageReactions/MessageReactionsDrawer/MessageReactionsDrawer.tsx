0540e4f3a6e6c196a2cf391f596b68a708cbef21
Louis Rouffineau <dev@louis.io>
2024-10-28 11:45:52 +0100

    Add `BottomSheetModalProvider`

--- components/Chat/Message/MessageReactions/MessageReactionsDrawer/MessageReactionsDrawer.tsx
+++ components/Chat/Message/MessageReactions/MessageReactionsDrawer/MessageReactionsDrawer.tsx
@@ -4,7 +4,10 @@ import { BottomSheetModal } from "@design-system/BottomSheet/BottomSheetModal";
 import { HStack } from "@design-system/HStack";
 import { ScrollView } from "@design-system/ScrollView";
 import { Text } from "@design-system/Text";
-import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import {
+  BottomSheetModalProvider,
+  BottomSheetScrollView,
+} from "@gorhom/bottom-sheet";
 import { useAppTheme } from "@theme/useAppTheme";
 import { memo, useCallback } from "react";
 import { TouchableHighlight } from "react-native";
@@ -28,52 +31,54 @@ export const MessageReactionsDrawer = memo(function MessageReactionsDrawer() {
   }, []);
 
   return (
-    <BottomSheetModal
-      onDismiss={handleDismiss}
-      ref={bottomSheetModalRef}
-      topInset={insets.top}
-      snapPoints={["50%", "100%"]}
-    >
-      <BottomSheetContentContainer>
-        <BottomSheetHeader title="Reactions" hasClose />
-        <BottomSheetScrollView>
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            style={{
-              paddingLeft: theme.spacing.lg,
-              flexDirection: "row",
-            }}
-          >
-            <Text>All {rolledUpReactions.totalReactions}</Text>
-            {Object.entries(rolledUpReactions.details).map(
-              ([emoji, details]) => (
-                <TouchableHighlight
-                  key={emoji}
-                  style={{
-                    marginRight: 8,
-                    padding: 8,
-                    borderRadius: theme.borderRadius.sm,
-                    backgroundColor: details.userReacted
-                      ? theme.colors.fill.minimal
-                      : theme.colors.background.raised,
-                  }}
-                  underlayColor={theme.colors.border.subtle}
-                  accessible
-                  accessibilityRole="button"
-                  accessibilityLabel={`${details.count} ${emoji} reactions`}
-                >
-                  <HStack style={{ alignItems: "center" }}>
-                    <Text>{emoji}</Text>
-                    <Text>{details.count}</Text>
-                  </HStack>
-                </TouchableHighlight>
-              )
-            )}
-            <HStack style={{ width: theme.spacing.xxl }} />
-          </ScrollView>
-        </BottomSheetScrollView>
-      </BottomSheetContentContainer>
-    </BottomSheetModal>
+    <BottomSheetModalProvider>
+      <BottomSheetModal
+        onDismiss={handleDismiss}
+        ref={bottomSheetModalRef}
+        topInset={insets.top}
+        snapPoints={["50%", "100%"]}
+      >
+        <BottomSheetContentContainer>
+          <BottomSheetHeader title="Reactions" hasClose />
+          <BottomSheetScrollView>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              style={{
+                paddingLeft: theme.spacing.lg,
+                flexDirection: "row",
+              }}
+            >
+              <Text>All {rolledUpReactions.totalReactions}</Text>
+              {Object.entries(rolledUpReactions.details).map(
+                ([emoji, details]) => (
+                  <TouchableHighlight
+                    key={emoji}
+                    style={{
+                      marginRight: 8,
+                      padding: 8,
+                      borderRadius: theme.borderRadius.sm,
+                      backgroundColor: details.userReacted
+                        ? theme.colors.fill.minimal
+                        : theme.colors.background.raised,
+                    }}
+                    underlayColor={theme.colors.border.subtle}
+                    accessible
+                    accessibilityRole="button"
+                    accessibilityLabel={`${details.count} ${emoji} reactions`}
+                  >
+                    <HStack style={{ alignItems: "center" }}>
+                      <Text>{emoji}</Text>
+                      <Text>{details.count}</Text>
+                    </HStack>
+                  </TouchableHighlight>
+                )
+              )}
+              <HStack style={{ width: theme.spacing.xxl }} />
+            </ScrollView>
+          </BottomSheetScrollView>
+        </BottomSheetContentContainer>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 });
