fa5c5fc76615725cbb30ca40dc22cf7ab70e10c5
Thierry <skodathierry@gmail.com>
2024-11-07 16:10:32 -0500

    fix polyfills order

--- polyfills.ts
+++ polyfills.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: DO NOT CHANGE THE ORDER OF THESE IMPORTS
 // These imports must be in this specific order for polyfills to work correctly
-import "@ethersproject/shims";
 import "@walletconnect/react-native-compat";
 import "react-native-get-random-values";
+import "@ethersproject/shims";
 
+import "text-encoding";
 import "@azure/core-asynciterator-polyfill";
+import "react-native-polyfill-globals/auto";
 import { Crypto as WebCrypto } from "@peculiar/webcrypto";
 import "@stardazed/streams-polyfill";
-import "react-native-polyfill-globals/auto";
-import "text-encoding";
 
 // Necessary for @peculiar/webcrypto.
 if (!global.Buffer) {
