0d6480f4265fc36334db9ceda37c1904bd120f56
Alex Risch <alex.j.risch@gmail.com>
2024-11-25 11:30:43 -0500

    fix: Test Updates
    Updated Tests to work on V3 Only

---
+++ __mocks__/@sentry/react-native.ts
@@ -0,0 +1,17 @@
+const init = jest.fn();
+const captureException = jest.fn();
+const captureMessage = jest.fn();
+const addBreadcrumb = jest.fn();
+const withScope = jest.fn();
+
+const MockedSentry = {
+  init,
+  captureException,
+  captureMessage,
+  addBreadcrumb,
+  withScope,
+};
+
+export { init, captureException, captureMessage, addBreadcrumb, withScope };
+
+export default MockedSentry;
