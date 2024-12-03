f3693f2957bc68b637ae3a2f1ca958afe9d44471
Alex Risch <alex.j.risch@gmail.com>
2024-12-03 14:16:28 -0500

    fix: Fixed Reply Bubble Color (#1286)
    Updated dark color for nested replies

--- theme/colorsDark.ts
+++ theme/colorsDark.ts
@@ -7,7 +7,7 @@ export const colorsDark: IColors = {
     bubbleAccent: darkPalette.accent,
     reply: lightPalette.light96,
     nestedReply: darkPalette.alpha4,
-    nestedReplyFromMe: darkPalette.dark85,
+    nestedReplyFromMe: darkPalette.alpha4,
     received: {
       bubble: lightPalette.light15,
       reply: lightPalette.light15,
