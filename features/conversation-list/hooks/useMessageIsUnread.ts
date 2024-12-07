d02db884b4a13e172c3c6b04fcd3dac9e6136742
Louis Rouffineau <dev@louis.io>
2024-12-07 08:38:58 +0100

    Fix

--- features/conversation-list/hooks/useMessageIsUnread.ts
+++ features/conversation-list/hooks/useMessageIsUnread.ts
@@ -1,29 +1,18 @@
 import { useMemo } from "react";
-import { ChatStoreType, TopicsData } from "@data/store/chatStore";
+import { ChatStoreType } from "@data/store/chatStore";
 import {
   ConversationWithCodecsType,
   DecodedMessageWithCodecsType,
 } from "@utils/xmtpRN/client";
-import {
-  currentAccount,
-  useChatStore,
-  useCurrentAccount,
-} from "@data/store/accountsStore";
+import { useChatStore } from "@data/store/accountsStore";
 import { useSelect } from "@data/store/storeHelpers";
 import { normalizeTimestamp } from "@/utils/date";
-import { useDmPeerInboxId } from "@/queries/useDmPeerInbox";
-import { useConversationCurrentTopic } from "@/features/conversation/conversation-service";
-import { ConversationTopic } from "@xmtp/react-native-sdk";
 
 type UseConversationIsUnreadProps = {
   topic: string;
   lastMessage: DecodedMessageWithCodecsType | undefined;
+  conversation: ConversationWithCodecsType;
   timestamp: number;
-  /**
-   * @deprecated This prop has been deprecated.
-   * for backward compatibility only
-   */
-  conversation?: ConversationWithCodecsType;
 };
 
 const chatStoreSelectKeys: (keyof ChatStoreType)[] = ["topicsData"];
@@ -31,24 +20,29 @@ const chatStoreSelectKeys: (keyof ChatStoreType)[] = ["topicsData"];
 export const useConversationIsUnread = ({
   topic,
   lastMessage,
+  conversation,
   timestamp: timestampNs,
 }: UseConversationIsUnreadProps) => {
-  const currentAccount = useCurrentAccount()!;
-
-  const validTopic: ConversationTopic = "valid-topic" as ConversationTopic;
-
-  const topicsData = useChatStore((state: ChatStoreType) => state.topicsData);
-  const { data: peerInboxId } = useDmPeerInboxId(currentAccount, validTopic);
+  const { topicsData } = useChatStore(useSelect(chatStoreSelectKeys));
 
   return useMemo(() => {
-    if (!lastMessage) return false;
-    if (topicsData[topic]?.status === "unread") return true;
-    // Don't mark as unread if message is from peer
-    if (lastMessage.senderAddress === peerInboxId) {
+    if (topicsData[topic]?.status === "unread") {
+      return true;
+    }
+    // Return false because current user sent the last message)
+    if (lastMessage?.senderAddress === conversation?.client.inboxId) {
       return false;
     }
     const timestamp = normalizeTimestamp(timestampNs);
     const readUntil = topicsData[topic]?.readUntil || 0;
+
+    // Return true if the last message timestamp is greater than readUntil
     return readUntil < (timestamp ?? 0);
-  }, [topicsData, topic, timestampNs, lastMessage, peerInboxId]);
+  }, [
+    topicsData,
+    topic,
+    lastMessage?.senderAddress,
+    conversation?.client.inboxId,
+    timestampNs,
+  ]);
 };
