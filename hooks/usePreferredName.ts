db691ecd1b5bcd978ec110f1c3101051066770a8
Alex Risch <alex.j.risch@gmail.com>
2024-12-09 19:15:07 -0500

    fix: Profile Name not showing (#1329)
    * fix: Profile Name not showing
    
    Removed some usages of old profiles store for queries
    Updated reused function to handle profile names
    
    * fix: Profile Name not showing
    
    Removed some usages of old profiles store for queries
    Updated reused function to handle profile names
    
    * fix typo

--- hooks/usePreferredName.ts
+++ hooks/usePreferredName.ts
@@ -1,8 +1,9 @@
 import { getPreferredName } from "@utils/profile";
 
 import { useProfileSocials } from "./useProfileSocials";
+import { shortAddress } from "@/utils/strings/shortAddress";
 
 export const usePreferredName = (peerAddress: string) => {
   const { data } = useProfileSocials(peerAddress);
-  return data ? getPreferredName(data, peerAddress) : peerAddress;
+  return data ? getPreferredName(data, peerAddress) : shortAddress(peerAddress);
 };
