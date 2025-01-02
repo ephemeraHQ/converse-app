0717e1b1cc4fb4db5f7a5b9808fba62898d8daaf
Michael Lustig <lustig@knophy.com>
2025-01-02 16:19:25 -0500

    make logger light color scheme friendly

--- utils/logger.ts
+++ utils/logger.ts
@@ -69,16 +69,29 @@ const converseTransport: transportFunctionType = async (props) => {
   await RNFS.appendFile(loggingFilePath, `${props.msg}\n`, "utf8");
 };
 
+const developerSystemAppearance: "dark" | "light" = "light" as const;
+const darkSystemColorScheme = {
+  debug: "white",
+  info: "blueBright",
+  warn: "yellowBright",
+  error: "redBright",
+};
+
+const lightSystemColorScheme = {
+  debug: "black",
+  info: "blue",
+  warn: "orange",
+  error: "red",
+};
+
 const _logger = RNLogger.createLogger({
   severity: "debug", // @todo => change minimum severity according to env & user (debug addresses)
   transport: converseTransport,
   transportOptions: {
-    colors: {
-      debug: "white",
-      info: "blueBright",
-      warn: "yellowBright",
-      error: "redBright",
-    },
+    colors:
+      developerSystemAppearance === "dark"
+        ? darkSystemColorScheme
+        : lightSystemColorScheme,
   },
   levels: {
     debug: 0,
