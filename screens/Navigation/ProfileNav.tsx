b08da001549c358b6fecd09d0c03982d64fab835
Louis Rouffineau <dev@louis.io>
2025-01-13 13:38:30 +0100

    WIP implement profile screen header with the design system along with share/copy/block options

--- screens/Navigation/ProfileNav.tsx
+++ screens/Navigation/ProfileNav.tsx
@@ -1,16 +1,7 @@
 import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
-import { textPrimaryColor, textSecondaryColor } from "@styles/colors";
-import { Platform, useColorScheme } from "react-native";
-
-import { ScreenHeaderButton } from "../../components/Screen/ScreenHeaderButton/ScreenHeaderButton";
-import { ScreenHeaderIconButton } from "../../components/Screen/ScreenHeaderIconButton/ScreenHeaderIconButton";
-import { useCurrentAccount } from "../../data/store/accountsStore";
-import { useRouter } from "../../navigation/useNavigation";
-import { navigate } from "../../utils/navigation";
 import ProfileScreen from "../Profile";
-import { NativeStack, navigationAnimation } from "./Navigation";
+import { NativeStack } from "./Navigation";
 import type { ConversationTopic } from "@xmtp/react-native-sdk";
-import { translate } from "@/i18n";
 
 export type ProfileNavParams = {
   address: string;
@@ -22,48 +13,13 @@ export const ProfileScreenConfig = {
 };
 
 export default function ProfileNav() {
-  const router = useRouter();
-  const colorScheme = useColorScheme();
-  const account = useCurrentAccount();
-  const options: NativeStackNavigationOptions = {
-    headerTintColor:
-      Platform.OS === "android"
-        ? textSecondaryColor(colorScheme)
-        : textPrimaryColor(colorScheme),
-    animation: navigationAnimation,
-  };
-
   return (
     <NativeStack.Screen
       name="Profile"
       component={ProfileScreen}
-      options={({ route }) => ({
+      options={{
         headerTitle: "",
-        headerRight: () => {
-          if (route.params.address === account) {
-            if (Platform.OS === "ios") {
-              return (
-                <ScreenHeaderButton
-                  title={translate("modify")}
-                  onPress={() => {
-                    navigate("UserProfile");
-                  }}
-                />
-              );
-            }
-            return (
-              <ScreenHeaderIconButton
-                iconName="square.and.pencil"
-                onPress={() => {
-                  navigate("UserProfile");
-                }}
-              />
-            );
-          }
-          return null;
-        },
-        ...options,
-      })}
+      }}
     />
   );
 }
