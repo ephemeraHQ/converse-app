36d3c5dbcd7fe42145a9b7a3224df4dc368685e8
Thierry <skodathierry@gmail.com>
2025-01-22 23:21:13 -0500

    better handling error

--- utils/xmtpRN/xmtp-preferences/xmtp-preferences.ts
+++ utils/xmtpRN/xmtp-preferences/xmtp-preferences.ts
@@ -1,6 +1,14 @@
+import logger from "@/utils/logger";
 import { getXmtpClient } from "../sync";
+import { captureError } from "@/utils/capture-error";
 
 export const syncConsent = async (account: string) => {
   const client = await getXmtpClient(account);
-  await client.preferences.syncConsent();
+  try {
+    await client.preferences.syncConsent();
+  } catch (error) {
+    captureError(error, {
+      caller: "syncConsent",
+    });
+  }
 };
