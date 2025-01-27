7c29ecb798566e25ebc2dbb1adf6939272dfe577
Louis Rouffineau <dev@louis.io>
2025-01-27 11:54:43 +0100

    Fixing logout

--- utils/logout/index.tsx
+++ utils/logout/index.tsx
@@ -1,7 +1,7 @@
 import { deleteLibXmtpDatabaseForInboxId } from "@utils/fileSystem";
 import logger from "@utils/logger";
 import { dropXmtpClient } from "@utils/xmtpRN/client";
-import { ConverseXmtpClientType } from "../xmtpRN/client.types";
+import { ConverseXmtpClientType } from "@utils/xmtpRN/client.types";
 import { getInboxId } from "@utils/xmtpRN/signIn";
 import { useCallback } from "react";
 
@@ -10,11 +10,17 @@ import {
   getWalletStore,
   TEMPORARY_ACCOUNT_NAME,
   useAccountsStore,
-} from "../../data/store/accountsStore";
-import { setAuthStatus } from "../../data/store/authStore";
-import { deleteSecureItemAsync } from "../keychain";
-import { deleteAccountEncryptionKey, deleteXmtpKey } from "../keychain/helpers";
-import mmkv, { clearSecureMmkvForAccount, secureMmkvByAccount } from "../mmkv";
+} from "@data/store/accountsStore";
+import { setAuthStatus } from "@data/store/authStore";
+import { deleteSecureItemAsync } from "@utils/keychain";
+import {
+  deleteAccountEncryptionKey,
+  deleteXmtpKey,
+} from "@utils/keychain/helpers";
+import mmkv, {
+  clearSecureMmkvForAccount,
+  secureMmkvByAccount,
+} from "@utils/mmkv";
 
 import { useDisconnectFromPrivy } from "./privy";
 import { deleteXmtpClient, getXmtpClient } from "../xmtpRN/sync";
@@ -145,95 +151,94 @@ export const logoutAccount = async (
   privyLogout: () => void
 ) => {
   logger.debug(
-    `[Logout] Logging out from ${account} with dropLocalDatabase=${dropLocalDatabase} and isV3Enabled=${isV3Enabled}`
+    `[Logout] Starting logout process for ${account} with dropLocalDatabase=${dropLocalDatabase} and isV3Enabled=${isV3Enabled}`
   );
+
+  // Get initial state for debugging
+  const initialCurrentAccount = useAccountsStore.getState().currentAccount;
+  logger.debug(`[Logout] Initial current account: ${initialCurrentAccount}`);
+
   if (isV3Enabled) {
     // This clears the libxmtp sqlite database (v3 / groups)
     try {
       const client = (await getXmtpClient(account)) as ConverseXmtpClientType;
       await client.dropLocalDatabaseConnection();
-      logger.debug("[Logout] successfully dropped connection to libxmp db");
+      logger.debug("[Logout] Successfully dropped connection to libxmp db");
       if (dropLocalDatabase) {
         await client.deleteLocalDatabase();
-        logger.debug("[Logout] successfully deleted libxmp db");
+        logger.debug("[Logout] Successfully deleted libxmp db");
         // Manual delete database files
         await deleteLibXmtpDatabaseForInboxId(client.inboxId);
       }
     } catch (error) {
-      logger.warn("Could not get XMTP Client while logging out", {
+      logger.warn("[Logout] Could not get XMTP Client while logging out", {
         error,
       });
     }
   }
+
   await dropXmtpClient((await getInboxId(account)) as InstallationId);
   const isPrivyAccount = !!useAccountsStore.getState().privyAccountId[account];
-  if (isPrivyAccount) {
-    privyLogout();
-  }
-  // const topicsByAccount: { [a: string]: string[] } = {};
-  const accounts = getAccountsList();
-  // accounts.forEach((a) => {
-  //   topicsByAccount[a] = Object.keys(getChatStore(a).getState().conversations);
-  // });
 
-  // We need to delete topics that are in this account and not other accounts
-  // so we start with topics from this account and we'll remove topics we find in others
-  // const topicsToDelete = topicsByAccount[account] || [];
-  const pkPath = getWalletStore(account).getState().privateKeyPath;
-
-  let apiHeaders: { [key: string]: string } | undefined;
-
-  try {
-    apiHeaders = await getXmtpApiHeaders(account);
-  } catch (error) {
-    // If we have a broken client we might not be able
-    // to generate the headers
-    logger.warn("Could not get API headers while logging out", {
-      error,
-    });
-  }
-
-  // accounts.forEach((a) => {
-  //   if (a !== account) {
-  //     topicsByAccount[a].forEach((topic) => {
-  //       const topicIndex = topicsToDelete.indexOf(topic);
-  //       if (topicIndex > -1) {
-  //         topicsToDelete.splice(topicIndex, 1);
-  //       }
-  //     });
-  //   }
-  // });
-
-  // Remove account so we don't use it anymore
-  useAccountsStore.getState().removeAccount(account);
-
-  // Set the new current account if we have one
-  // New current account doesn't change if it's not the one to remove,
-  // else we find the first non temporary one and fallback to temporary (= logout)
-  const currentAccount = useAccountsStore.getState().currentAccount;
-  const setCurrentAccount = useAccountsStore.getState().setCurrentAccount;
-  if (currentAccount === account) {
-    const nonTemporaryAccount = accounts.find(
-      (a) => a !== TEMPORARY_ACCOUNT_NAME && a !== account
-    );
-    if (nonTemporaryAccount) {
-      setCurrentAccount(nonTemporaryAccount, false);
-    } else {
-      setCurrentAccount(TEMPORARY_ACCOUNT_NAME, false);
-      // No more accounts, let's go back to onboarding
-      setAuthStatus("signedOut");
-    }
-  }
-
+  // Clean up all account-related data first
   deleteXmtpClient(account);
   deleteSubscribedTopics(account);
   delete secureMmkvByAccount[account];
   delete lastNotifSubscribeByAccount[account];
 
-  // TODO: Set topics to delete
+  // Remove account from store
+  logger.debug(`[Logout] Removing account ${account} from store`);
+  useAccountsStore.getState().removeAccount(account);
+
+  // Get the updated list of accounts after removal
+  const remainingAccounts = getAccountsList();
+  logger.debug(
+    `[Logout] Remaining accounts after removal: ${JSON.stringify(
+      remainingAccounts
+    )}`
+  );
+
+  // Important: Set auth status to signedOut BEFORE changing current account
+  // This ensures navigation state is reset before any account changes
+  if (dropLocalDatabase) {
+    logger.debug(
+      "[Logout] Setting auth status to signedOut due to dropLocalDatabase"
+    );
+    setAuthStatus("signedOut");
+  }
+
+  // Set temporary account
+  const setCurrentAccount = useAccountsStore.getState().setCurrentAccount;
+  setCurrentAccount(TEMPORARY_ACCOUNT_NAME, false);
+  logger.debug(`[Logout] Set temporary account: ${TEMPORARY_ACCOUNT_NAME}`);
+
+  // Execute Privy logout last to prevent any race conditions
+  if (isPrivyAccount) {
+    logger.debug("[Logout] Executing Privy logout for account");
+    privyLogout();
+  }
+
+  // Save logout task
+  const pkPath = getWalletStore(account).getState().privateKeyPath;
+  let apiHeaders: { [key: string]: string } | undefined;
+  try {
+    apiHeaders = await getXmtpApiHeaders(account);
+  } catch (error) {
+    logger.warn("[Logout] Could not get API headers while logging out", {
+      error,
+    });
+  }
   saveLogoutTask(account, apiHeaders, [], pkPath);
 
+  // Add a final state check
   setTimeout(() => {
+    const finalState = {
+      currentAccount: useAccountsStore.getState().currentAccount,
+      remainingAccounts: getAccountsList(),
+    };
+    logger.debug(
+      `[Logout] Final state after logout: ${JSON.stringify(finalState)}`
+    );
     executeLogoutTasks();
   }, 500);
 };
