870a8bb402ca6835652e2fa86b3974536ad1206a
Thierry Skoda <6875849+thierryskoda@users.noreply.github.com>
2025-02-06 21:02:32 -0500

    fix: Privy ("Already used" error) (#1599)

---
+++ scripts/android/build/android-deps-expo-plugin.js
@@ -0,0 +1,60 @@
+/**
+ * This work was done by Noe.
+ * It handles some conflicting dependencies manually.
+ * It seems to be needed for now for Android build to work with eas.
+ * We might be able to remove it once we will clean up our dependencies?
+ * Maybe not since it's used by many crypto libs
+ * https://github.com/expo/expo/issues/29026#issuecomment-2173524698
+ */
+const {
+  createRunOncePlugin,
+  withAppBuildGradle,
+} = require("@expo/config-plugins");
+const {
+  mergeContents,
+} = require("@expo/config-plugins/build/utils/generateCode");
+
+const withFixedConverseAndroidDependencies = (config) => {
+  return withAppBuildGradle(config, (config) => {
+    if (config.modResults.contents) {
+      config.modResults.contents = setBouncyCastleVersion(
+        config.modResults.contents
+      );
+    }
+    return config;
+  });
+};
+
+const TAG = "converse-android-dependencies-fix";
+
+const setBouncyCastleVersion = (src) => {
+  const configurationFix = `
+      configurations.all {
+    resolutionStrategy.eachDependency { DependencyResolveDetails details ->
+        if (details.requested.name == 'bcprov-jdk15on') {
+            details.useTarget group: details.requested.group, name: 'bcprov-jdk15to18', version: '1.78.1'
+        }
+        if (details.requested.name == 'lifecycle-viewmodel-ktx') {
+            details.useTarget group: details.requested.group, name: 'lifecycle-viewmodel-ktx', version: '2.5.0'
+        }
+    }
+  }
+  `;
+
+  const mergeResults = mergeContents({
+    tag: TAG,
+    src: src,
+    newSrc: configurationFix,
+    anchor: /android {/gm,
+    offset: 1,
+    comment: `// `,
+  });
+
+  return mergeResults.contents;
+};
+
+module.exports = createRunOncePlugin(
+  withFixedConverseAndroidDependencies,
+  "withFixedConverseAndroidDependencies",
+  "1.0.0"
+);
