df67659fe1c4b18d26b42ee26f9b2ce8131dd9ab
Michael Lustig <lustig@knophy.com>
2025-02-11 12:15:30 -0500

    new create user endpoint

--- utils/api/users.ts
+++ utils/api/users.ts
@@ -1,14 +1,7 @@
 import { Platform } from "react-native";
-import {
-  analyticsAppVersion,
-  analyticsBuildNumber,
-  analyticsPlatform,
-} from "../analytics";
-import { api, oldApi } from "./api";
+import { api } from "./api";
 import { getXmtpApiHeaders } from "./auth";
 import * as Device from "expo-device";
-
-const lastSaveUser: { [address: string]: number } = {};
 
 export const createUser = async (args: {
   privyUserId: string;
@@ -16,35 +9,17 @@ export const createUser = async (args: {
   inboxId: string;
 }) => {
   const { privyUserId, smartContractWalletAddress, inboxId } = args;
-  const { data } = await api.post("/api/v1/users", {
-    privyUserId,
-    privyAddress: smartContractWalletAddress,
-    inboxId,
-    deviceOS: Platform.OS.toLowerCase(),
-    deviceName: Device.modelName,
-  });
-  return data;
-};
 
-export const saveUser = async (args: { address: string }) => {
-  const { address } = args;
-  const now = new Date().getTime();
-  const last = lastSaveUser[address] || 0;
-  if (now - last < 3000) {
-    // Avoid race condition when changing account at same
-    // time than coming back on the app.
-    return;
-  }
-  lastSaveUser[address] = now;
-
-  await oldApi.post(
-    "/api/user",
+  await api.post(
+    "/api/user/create",
     {
-      address,
-      platform: analyticsPlatform,
-      version: analyticsAppVersion,
-      build: analyticsBuildNumber,
+      inboxId,
+      address: smartContractWalletAddress,
+      privyId: privyUserId,
+      deviceName:
+        /* todo get we get the ios entgitlement to get "vivians iphone"*/ Device.modelId,
+      os: Platform.OS.toLowerCase(),
     },
-    { headers: await getXmtpApiHeaders(address) }
+    { headers: await getXmtpApiHeaders(smartContractWalletAddress) }
   );
 };
