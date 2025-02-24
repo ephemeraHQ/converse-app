fa562d5fe6cd30def020b2bee70ea7f839517694
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-02-24 20:55:27 +0100

    Update WebviewPreview.tsx

--- screens/WebviewPreview.tsx
+++ screens/WebviewPreview.tsx
@@ -9,7 +9,7 @@ export function WebviewPreview({
   // This is a trick because on Android, the setAllowFileAccess method
   // was called AFTER the setAllowFileAccess method and first file
   // uri could not be loaded. That way we start with a blank screen and
-  // immediatly after we load our local URI
+  // immediately after we load our local URI
   const [uri, setUri] = useState("");
   useEffect(() => {
     setImmediate(() => {
